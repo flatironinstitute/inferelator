1140ac210195388173b08cb0bc8bffa5109f88d0
Aaron Watters <awatters@simonsfoundation.org>
2016-06-10 09:42:20 -0400

    add design response for condition (not in timeseries)

--- inferelator_strawman/condition.py
+++ inferelator_strawman/condition.py
@@ -20,3 +20,11 @@ class Condition:
     def __init__(self, condition_name, gene_mapping):
         self.name = condition_name
         self.gene_mapping = pd.Series(gene_mapping)
+
+    def response_scalar(self, gene_name):
+        "Return the gene 'response' for this conditon."
+        return self.gene_mapping[gene_name]
+
+    def design_vector(self, transcription_factors):
+        "Return a 1d array of transcription factor coefficients for condition."
+        return self.gene_mapping[transcription_factors]
--- inferelator_strawman/tests/test_condition.py
+++ inferelator_strawman/tests/test_condition.py
@@ -7,12 +7,14 @@ class TestCondition(unittest.TestCase):
 
     def test_convert_dictionary(self):
         "Test that a dictionary works as condition data."
-        D = {"gene1": 3}
+        D = {"gene1": 3, "tf1": 6, "tf2": -1}
         c = condition.Condition("my condition", D)
         self.assertEqual(c.name, "my condition")
         m = c.gene_mapping
         self.assertEqual(type(m), pd.Series)
         self.assertEqual(m.to_dict(), D)
+        self.assertEqual(c.response_scalar("gene1"), D["gene1"])
+        self.assertEqual(list(c.design_vector(["tf1", "tf2"])), [D["tf1"], D["tf2"]])
 
     def test_cant_convert(self):
         with self.assertRaises(Exception):
