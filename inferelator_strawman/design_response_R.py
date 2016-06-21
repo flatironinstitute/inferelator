2ff92ed470f06847a62aa101ecbc51cc738034f4
Aaron Watters <awatters@simonsfoundation.org>
2016-06-21 11:36:55 -0400

    design and response test case

--- inferelator_strawman/design_response_R.py
+++ inferelator_strawman/design_response_R.py
@@ -3,6 +3,8 @@ Compute design and response by calling R subprocess.
 """
 
 import os
+import subprocess
+import pandas as pd
 
 my_dir = os.path.dirname(__file__)
 
@@ -22,15 +24,56 @@ dr <- design.and.response(meta.data, exp.mat, delT.min, delT.max, tau)
 dr$final_response_matrix
 dr$final_design_matrix
 
-write.table(as.matrix(dr$final_response_matrix), 'response.tsv', sep = '\t')
-write.table(as.matrix(dr$final_design_matrix), 'design.tsv', sep = '\t')
+write.table(as.matrix(dr$final_response_matrix), '{response_file}', sep = '\t')
+write.table(as.matrix(dr$final_design_matrix), '{design_file}', sep = '\t')
 """
 
 def save_R_driver(to_filename, delTmin=0, delTmax=110, tau=45,
         meta_file="meta_data.csv", exp_file="exp_mat.csv",
-        module=DR_module):
+        module=DR_module, response_file='response.tsv', design_file='design.tsv'):
     assert os.path.exists(DR_module), "doesn't exist " + repr(DR_module)
     text = R_template.format(delTmin=delTmin, delTmax=delTmax, tau=tau,
-                meta_file=meta_file, exp_file=exp_file, module=module)
+                meta_file=meta_file, exp_file=exp_file, module=module,
+                response_file=response_file, design_file=design_file)
     with open(to_filename, "w") as outfile:
         outfile.write(text)
+    return (to_filename, design_file, response_file)
+
+class DR_driver:
+
+    """
+    Configurable container for calling R subprocess to
+    compute design and response.
+    """
+
+    target_directory = "/tmp"
+    meta_file = "meta_data.csv"
+    exp_file = "exp_mat.csv"
+    script_file = "run_design_response.R"
+    response_file = "response.tsv"
+    design_file = "design.tsv"
+    delTmin = 0
+    delTmax = 110
+    tau = 45
+
+    def path(self, filename):
+        return os.path.join(self.target_directory, filename)
+
+    def run(self, expression_data_frame, metadata_dataframe):
+        expression_data_frame.to_csv(self.path(self.exp_file))
+        metadata_dataframe.to_csv(self.path(self.meta_file))
+        (driver_path, design_path, response_path) = save_R_driver(
+            to_filename=self.path(self.script_file),
+            delTmin=self.delTmin,
+            delTmax=self.delTmax,
+            tau=self.tau,
+            meta_file=self.path(self.meta_file),
+            exp_file=self.path(self.exp_file),
+            response_file=self.path(self.response_file),
+            design_file=self.path(self.design_file)
+        )
+        subprocess.call(['R', '-f', driver_path])
+        final_design = pd.read_csv(design_path, sep='\t')
+        final_response = pd.read_csv(response_path, sep='\t')
+        return (final_design, final_response)
+
--- inferelator_strawman/tests/test_design_response_R.py
+++ inferelator_strawman/tests/test_design_response_R.py
@@ -1,4 +1,6 @@
 import unittest, os
+import pandas as pd
+import numpy as np
 from .. import design_response_R
 
 my_dir = os.path.dirname(__file__)
@@ -13,3 +15,25 @@ class TestDRR(unittest.TestCase):
         text = open(outpath).read()
         assert "tau <- 101" in text
         # XXXX could clean up artifact, for now useful for debugging
+
+    def test_DR_driver(self):
+        drd = design_response_R.DR_driver()
+        drd.target_directory = os.path.join(my_dir, "artifacts")
+        meta = pd.DataFrame()
+        meta['isTs']=[True, True, True, True, False]
+        meta['is1stLast'] = ['f','m','m','l','e']
+        meta['prevCol'] = ['NA','ts1','ts2','ts3', 'NA']
+        meta['del.t'] = ['NA', 3, 2, 5, 'NA']
+        meta['condName'] = ['ts1','ts2','ts3','ts4','ss']
+
+        exp = pd.DataFrame(np.reshape(range(10), (2,5)) + 1,
+         index = ['gene' + str(i + 1) for i in range(2)],
+         columns = ['ts' + str(i + 1) for i in range(4)] + ['ss'])
+        drd.delT_min = 2
+        drd.delT_max = 4
+        drd.tau = 2
+        (design, response) = drd.run(exp, meta)
+        print "design"
+        print design
+        print "response"
+        print response
