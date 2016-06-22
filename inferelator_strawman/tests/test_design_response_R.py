6b035cee4ce5bb3fc0afa94f86dcf10bdc8d3515
Aaron Watters <awatters@simonsfoundation.org>
2016-06-22 15:01:14 -0400

    branching tests added

--- inferelator_strawman/tests/test_design_response_R.py
+++ inferelator_strawman/tests/test_design_response_R.py
@@ -101,36 +101,41 @@ class TestDRBelowDeltMin(TestDR):
         self.assertEqual(list(ds.columns), ['ss', 'ts1', 'ts2', 'ts3'], 
             msg = "Guarantee that the ts4 condition is dropped, since its the last in the time series")
 
+class TestBranchingTimeSeries(TestDR):
 
-class TestDRR(unittest.TestCase):
+    def setUp(self):
+        self.meta = pd.DataFrame()
+        self.meta['isTs']=[True, True, True]
+        self.meta['is1stLast'] = ['f','l','l']
+        self.meta['prevCol'] = ['NA','ts1','ts1']
+        self.meta['del.t'] = ['NA', 2, 2]
+        self.meta['condName'] = ['ts1','ts2','ts3']
+        self.exp = pd.DataFrame(np.reshape(range(9), (3,3)) + 1,
+            index = ['gene' + str(i + 1) for i in range(3)],
+            columns = ['ts' + str(i + 1) for i in range(3)])
+        self.delT_min = 1
+        self.delT_max = 4
+        self.tau = 1
+        self.calculate_design_and_response()
 
-    def xtest_save_R_driver(self):
-        outfile = "artifacts/test_R_driver.R"
-        outpath = os.path.join(my_dir, outfile)
-        design_response_R.save_R_driver(outpath, tau=101)
-        assert os.path.exists(outpath)
-        text = open(outpath).read()
-        assert "tau <- 101" in text
-        # XXXX could clean up artifact, for now useful for debugging
+    def test_design_matrix_branching_time_series(self):
+        ds, resp = (self.design, self.response)
+        self.assertEqual(ds.shape, (3, 2))
+        self.assertEqual(list(ds.columns), ['ts1_dupl01', 'ts1_dupl02'],
+             msg = 'This is how the R code happens to name branching time series')
+        for col in ds:
+            self.assertEqual(list(ds[col]), list(self.exp['ts1']), 
+                msg = '{} column in the design matrix should be equal to the branching source, ts1, in the exp matrix'.format(col))
 
-    def xtest_DR_driver(self):
-        drd = design_response_R.DR_driver()
-        drd.target_directory = os.path.join(my_dir, "artifacts")
-        meta = pd.DataFrame()
-        meta['isTs']=[True, True, True, True, False]
-        meta['is1stLast'] = ['f','m','m','l','e']
-        meta['prevCol'] = ['NA','ts1','ts2','ts3', 'NA']
-        meta['del.t'] = ['NA', 3, 2, 5, 'NA']
-        meta['condName'] = ['ts1','ts2','ts3','ts4','ss']
+    def test_response_matrix_branching_time_series(self):
+        ds, resp = (self.design, self.response)
+        self.assertEqual(resp.shape, (3, 2))
+        expression_1 = np.array(list(self.exp['ts1']))
+        expression_2 = np.array(list(self.exp['ts2']))
+        expected_response_1 = expression_1 + self.tau * (expression_2 - expression_1) /  float(self.meta['del.t'][1])
 
-        exp = pd.DataFrame(np.reshape(range(10), (2,5)) + 1,
-         index = ['gene' + str(i + 1) for i in range(2)],
-         columns = ['ts' + str(i + 1) for i in range(4)] + ['ss'])
-        drd.delT_min = 2
-        drd.delT_max = 4
-        drd.tau = 2
-        (design, response) = drd.run(exp, meta)
-        print ("design")
-        print (design)
-        print ("response")
-        print (response)
+        expression_3 = np.array(list(self.exp['ts3']))
+        expected_response_2 = expression_1 + self.tau * (expression_3 - expression_1) /  float(self.meta['del.t'][2])
+
+        np.testing.assert_almost_equal(np.array(resp['ts1_dupl01']), expected_response_1)
+        np.testing.assert_almost_equal(np.array(resp['ts1_dupl02']), expected_response_2)
