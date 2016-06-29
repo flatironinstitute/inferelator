efec42b1fc0391736afd3e245e1f912afdd8a566
Nick De Veaux <nrdeveaux@gmail.com>
2016-06-29 17:42:12 +0200

    tests for one gene and two genes

--- inferelator_ng/tests/test_bbsr_R.py
+++ inferelator_ng/tests/test_bbsr_R.py
@@ -1,6 +1,8 @@
 import unittest, os
 import pandas as pd
+import pandas.util.testing as pdt
 import numpy as np
+from subprocess import CalledProcessError
 from .. import bbsr_R
 
 my_dir = os.path.dirname(__file__)
@@ -9,13 +11,24 @@ class TestDR(unittest.TestCase):
     """
     Superclass for common methods
     """
-    def test_calculate_design_and_response(self):
+    def test_two_gene(self):
         brd = bbsr_R.BBSR_driver()
         brd.target_directory = os.path.join(my_dir, "artifacts")
-        self.X = pd.DataFrame([1 ,2, 3])
-        self.Y = pd.DataFrame([1])
-        self.priors = pd.DataFrame([])
-        self.clr = pd.DataFrame([1 ,2, 3])
-        (i, j) = brd.run(self.X, self.Y, self.clr, self.priors)
-        print i
-        print j
+        self.X = pd.DataFrame([0, 0], index = ['gene1', 'gene2'], columns = ['ss'])
+        self.Y = pd.DataFrame([0, 0], index = ['gene1', 'gene2'], columns = ['ss'])
+        self.priors =  pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
+        self.clr = pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
+        (betas, resc) = brd.run(self.X, self.Y, self.clr, self.priors)
+        self.assertEqual(betas.shape, (2, 2))
+        self.assertEqual(resc.shape, (2, 2))
+        pdt.assert_frame_equal(betas, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
+        pdt.assert_frame_equal(resc, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
+
+    def test_fails_with_one_gene(self):
+        brd = bbsr_R.BBSR_driver()
+        brd.target_directory = os.path.join(my_dir, "artifacts")
+        self.X = pd.DataFrame([0], index = ['gene1'], columns = ['ss'])
+        self.Y = pd.DataFrame([0], index = ['gene1'], columns = ['ss'])
+        self.priors = pd.DataFrame([0], index = ['gene1'], columns = ['gene1'])
+        self.clr = pd.DataFrame([0], index = ['gene1'], columns = ['gene1'])
+        self.assertRaises(CalledProcessError, brd.run, self.X, self.Y, self.clr, self.priors)
