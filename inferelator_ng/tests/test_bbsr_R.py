4367ec6557786eb7eaca5b42e4cd02084df60984
Nick De Veaux <nrdeveaux@gmail.com>
2016-07-06 05:55:23 -0400

    pulling out shared functionality in test_bbsr_R to make it more modular

--- inferelator_ng/tests/test_bbsr_R.py
+++ inferelator_ng/tests/test_bbsr_R.py
@@ -8,41 +8,47 @@ from .. import bbsr_R
 my_dir = os.path.dirname(__file__)
 
 class TestDR(unittest.TestCase):
-    """
-    Superclass for common methods
-    """
+
+    def setUp(self):
+        self.brd = bbsr_R.BBSR_driver()
+        self.brd.target_directory = os.path.join(my_dir, "artifacts")
+
+    def set_all_zero_priors(self):
+        self.priors =  pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
+
+    def set_all_zero_clr(self):
+        self.clr = pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
+
+    def assert_matrix_is_square(self, size, matrix):
+        self.assertEqual(matrix.shape, (size, size))
+
     def test_two_genes(self):
-        brd = bbsr_R.BBSR_driver()
-        brd.target_directory = os.path.join(my_dir, "artifacts")
+        self.set_all_zero_priors()
+        self.set_all_zero_clr()
         self.X = pd.DataFrame([0, 0], index = ['gene1', 'gene2'], columns = ['ss'])
         self.Y = pd.DataFrame([0, 0], index = ['gene1', 'gene2'], columns = ['ss'])
-        self.priors =  pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
-        self.clr = pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
-        (betas, resc) = brd.run(self.X, self.Y, self.clr, self.priors)
-        self.assertEqual(betas.shape, (2, 2))
-        self.assertEqual(resc.shape, (2, 2))
+        
+        (betas, resc) = self.brd.run(self.X, self.Y, self.clr, self.priors)
+        self.assert_matrix_is_square(2, betas)
+        self.assert_matrix_is_square(2, resc)
         pdt.assert_frame_equal(betas, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
         pdt.assert_frame_equal(resc, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
 
     def test_fails_with_one_gene(self):
-        brd = bbsr_R.BBSR_driver()
-        brd.target_directory = os.path.join(my_dir, "artifacts")
+        self.set_all_zero_priors()
+        self.set_all_zero_clr()
         self.X = pd.DataFrame([0], index = ['gene1'], columns = ['ss'])
         self.Y = pd.DataFrame([0], index = ['gene1'], columns = ['ss'])
-        self.priors = pd.DataFrame([0], index = ['gene1'], columns = ['gene1'])
-        self.clr = pd.DataFrame([0], index = ['gene1'], columns = ['gene1'])
-        self.assertRaises(CalledProcessError, brd.run, self.X, self.Y, self.clr, self.priors)
+        self.assertRaises(CalledProcessError, self.brd.run, self.X, self.Y, self.clr, self.priors)
 
     def test_two_genes_nonzero(self):
-        brd = bbsr_R.BBSR_driver()
-        brd.target_directory = os.path.join(my_dir, "artifacts")
+        self.set_all_zero_priors()
+        self.set_all_zero_clr()
         self.X = pd.DataFrame([1, 2], index = ['gene1', 'gene2'], columns = ['ss'])
         self.Y = pd.DataFrame([1, 2], index = ['gene1', 'gene2'], columns = ['ss'])
-        self.priors =  pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
-        self.clr = pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
-        (betas, resc) = brd.run(self.X, self.Y, self.clr, self.priors)
-        self.assertEqual(betas.shape, (2, 2))
-        self.assertEqual(resc.shape, (2, 2))
+        (betas, resc) = self.brd.run(self.X, self.Y, self.clr, self.priors)
+        self.assert_matrix_is_square(2, betas)
+        self.assert_matrix_is_square(2, resc)
         pdt.assert_frame_equal(betas, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
         pdt.assert_frame_equal(resc, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
 
@@ -61,40 +67,34 @@ class TestDR(unittest.TestCase):
             0     0 
     """)
     def test_two_genes_nonzero_clr_nonzero(self):
-        brd = bbsr_R.BBSR_driver()
-        brd.target_directory = os.path.join(my_dir, "artifacts")
+        self.set_all_zero_priors()
         self.X = pd.DataFrame([1, 2], index = ['gene1', 'gene2'], columns = ['ss'])
         self.Y = pd.DataFrame([1, 2], index = ['gene1', 'gene2'], columns = ['ss'])
-        self.priors =  pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
         self.clr = pd.DataFrame([[.1, .1],[.1, .2]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
-        (betas, resc) = brd.run(self.X, self.Y, self.clr, self.priors)
-        self.assertEqual(betas.shape, (2, 2))
-        self.assertEqual(resc.shape, (2, 2))
+        (betas, resc) = self.brd.run(self.X, self.Y, self.clr, self.priors)
+        self.assert_matrix_is_square(2, betas)
+        self.assert_matrix_is_square(2, resc)
         pdt.assert_frame_equal(betas, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
         pdt.assert_frame_equal(resc, pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
 
     def test_two_genes_nonzero_clr_nonzero_two_conditions(self):
-        brd = bbsr_R.BBSR_driver()
-        brd.target_directory = os.path.join(my_dir, "artifacts")
+        self.set_all_zero_priors()
         self.X = pd.DataFrame([[1, 2], [2, 1]], index = ['gene1', 'gene2'], columns = ['ss1', 'ss2'])
         self.Y = pd.DataFrame([[1, 2], [2, 1]], index = ['gene1', 'gene2'], columns = ['ss1', 'ss2'])
-        self.priors =  pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
         self.clr = pd.DataFrame([[.1, .1],[.1, .2]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
-        (betas, resc) = brd.run(self.X, self.Y, self.clr, self.priors)
-        self.assertEqual(betas.shape, (2, 2))
-        self.assertEqual(resc.shape, (2, 2))
+        (betas, resc) = self.brd.run(self.X, self.Y, self.clr, self.priors)
+        self.assert_matrix_is_square(2, betas)
+        self.assert_matrix_is_square(2, resc)
         pdt.assert_frame_equal(betas, pd.DataFrame([[0, -1],[-1, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
         pdt.assert_frame_equal(resc, pd.DataFrame([[0, 1],[1, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
 
     def test_two_genes_nonzero_clr_nonzero_two_conditions_zero_gene1(self):
-        brd = bbsr_R.BBSR_driver()
-        brd.target_directory = os.path.join(my_dir, "artifacts")
+        self.set_all_zero_priors()
         self.X = pd.DataFrame([[0, 2], [2, 0]], index = ['gene1', 'gene2'], columns = ['ss1', 'ss2'])
         self.Y = pd.DataFrame([[0, 1], [1, 0]], index = ['gene1', 'gene2'], columns = ['ss1', 'ss2'])
-        self.priors =  pd.DataFrame([[0, 0],[0, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
         self.clr = pd.DataFrame([[.1, .1],[.1, .2]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2'])
-        (betas, resc) = brd.run(self.X, self.Y, self.clr, self.priors)
-        self.assertEqual(betas.shape, (2, 2))
-        self.assertEqual(resc.shape, (2, 2))
+        (betas, resc) = self.brd.run(self.X, self.Y, self.clr, self.priors)
+        self.assert_matrix_is_square(2, betas)
+        self.assert_matrix_is_square(2, resc)
         pdt.assert_frame_equal(betas, pd.DataFrame([[0, -1],[-1, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
         pdt.assert_frame_equal(resc, pd.DataFrame([[0, 1],[1, 0]], index = ['gene1', 'gene2'], columns = ['gene1', 'gene2']))
