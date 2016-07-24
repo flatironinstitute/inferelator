004774afcef5c165da17d8f64cb3eb1c54022297
ryi06 <ryi06@hotmail.com>
2016-07-24 17:04:11 -0400

    TFA refactor class

--- inferelator_ng/tfa2.py
+++ inferelator_ng/tfa2.py
@@ -26,35 +26,34 @@ class TFA:
 		By default, self interactions for TFs that other TFs with the 
 		exact same set of interactions in the prior are kept
 	"""
-
-	def __init__(self, prior, exp_mat, exp_mat_halftau):
+	"""
+	def __init__(self, prior, exp_mat):
 		self.prior = prior
 		self.exp_mat = exp_mat
-		self.exp_mat = exp_mat_halftau
-
-	def tfa(self, noself = True, dup_self = True):
-	    tfwt = self.prior.abs().sum(axis = 0) > 0
+	"""
+	def tfa(prior, exp_mat, exp_mat_halftau, noself = True, dup_self = True):
+	    tfwt = prior.abs().sum(axis = 0) > 0
 	    tfwt = tfwt[tfwt].index.tolist()
 
 	    if dup_self:
 	        # subset of prior matrix whose columns are not all zeros (snz: sum non-zero)
-	        prior_snz = self.prior[tfwt]			
+	        prior_snz = prior[tfwt]			
 	        duplicates = prior_snz.transpose().duplicated(keep=False) # mark duplicates as true
 	        dTFs = duplicates[duplicates].index.tolist()
 
 	    # find non-duplicated TFs that are also present in target gene list 
-	    ndTFs = list(set(self.prior.columns.values.tolist()).difference(dTFs))
+	    ndTFs = list(set(prior.columns.values.tolist()).difference(dTFs))
 
 	    if noself:
-	        selfTFs = list(set(ndTFs).intersection(self.prior.index.values.tolist()))
+	        selfTFs = list(set(ndTFs).intersection(prior.index.values.tolist()))
 	        prior.loc[selfTFs, selfTFs] = 0
 
-	    activity = pd.DataFrame(0, index = self.prior.columns, columns = self.exp_mat_halftau.columns)
+	    activity = pd.DataFrame(0, index = prior.columns, columns = exp_mat_halftau.columns)
 
 	    if any(tfwt):
 	        activity.loc[tfwt,:] = np.matrix(linalg.pinv2(prior_snz))* np.matrix(expr)
 
-	    ntfwt = list(set(self.prior.columns.values.tolist()).difference(tfwt))
+	    ntfwt = list(set(prior.columns.values.tolist()).difference(tfwt))
 	    activity.loc[ntfwt,:] = expr.loc[ntfwt,:]
 
 	    return activity
