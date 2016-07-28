ca5d8748dd3c9faea404f20d672937c833de136f
Aaron Watters <awatters@simonsfoundation.org>
2016-07-28 16:19:00 -0400

    add workflow skeleton

---
+++ inferelator_ng/tests/test_workflow_stub.py
@@ -0,0 +1,76 @@
+"""
+Test workflow logic outline using completely
+artificial stubs for dependancies.
+"""
+
+import unittest
+from .. import workflow
+import os
+
+my_dir = os.path.dirname(__file__)
+
+class StubWorkflow(workflow.WorkflowBase):
+
+    """
+    Artificial work flow for logic testing.
+    """
+
+    cd_called = False
+    test_case = None
+
+    def compute_common_data(self):
+        cd_called = True
+
+    def get_priors(self):
+        return [StubPrior(1), StubPrior(2)]
+
+    def get_bootstraps(self):
+        return [StubBootstrap("A"), StubBootstrap("B")]
+
+    def emit_results(self, priors):
+        # check that everything got initialized properly
+        assert self.exp_mat is not None
+        assert self.meta_data is not None
+        assert self.tf_names is not None
+        assert self.priors_data is not None
+        assert self.gold_standard is not None
+        test = self.test_case
+        idents = [1, 2]
+        names = ["A", "B"]
+        test.assertEqual([p.ident for p in priors], idents)
+        for p in priors:
+            test.assertEqual(p.names, names)
+
+class StubBootstrap(object):
+
+    def __init__(self, name):
+        self.name = name
+
+class StubPrior(object):
+
+    """
+    Completely artificial prior object shell implementation.
+    """
+
+    def __init__(self, ident):
+        self.ident = ident
+
+    names = []
+
+    def run_bootstrap(self, workflow, bootstrap_parameters):
+        self.names = self.names + [bootstrap_parameters.name]
+
+    bootstrap_results = None
+
+    def combine_bootstrap_results(self, workflow, results):
+        self.bootstrap_results = results
+
+class TestWorkflowStub(unittest.TestCase):
+
+    def test_stub(self):
+        # create and configure the work flow
+        work = StubWorkflow()
+        work.input_dir = os.path.join(my_dir, "../../data/dream4")
+        work.test_case = self
+        # run the workflow (validation tests in emit_results)
+        work.run()
---
+++ inferelator_ng/workflow.py
@@ -0,0 +1,96 @@
+"""
+Base implementation for high level workflow.
+
+The goal of this design is to make it easy to share
+code among different variants of the Inferelator workflow.
+"""
+
+from . import utils
+import numpy as np
+import os
+
+class WorkflowBase(object):
+
+    # Common configuration parameters
+    input_dir = None
+    exp_mat_file = "expression.tsv"
+    tf_names_file = "tf_names.tsv"
+    meta_data_file = "meta_data.tsv"
+    priors_file = "gold_standard.tsv"
+    gold_standard_file = "gold_standard.tsv"
+    random_seed = 42
+
+    # Computed data structures
+    exp_mat = None  # exp_mat dataframe
+    tf_names = None  # tf_names list
+    meta_data = None  # meta data dataframe
+    priors_data = None  # priors data dataframe
+    gold_standard = None  # gold standard dataframe
+
+    def __init__(self):
+        # Do nothing (all configuration is external to init)
+        pass
+
+    def run(self):
+        "Execute workflow, after all configuration."
+        self.get_data()
+        np.random.seed(self.random_seed)
+        self.compute_common_data()
+        priors = list(self.get_priors())
+        bootstraps = list(self.get_bootstraps())
+        for prior in priors:
+            results = [prior.run_bootstrap(self, bootstrap_parameters)
+                       for bootstrap_parameters in bootstraps]
+            prior.combine_bootstrap_results(self, results)
+        self.emit_results(priors)
+
+    def get_data(self):
+        self.exp_mat = self.input_dataframe(self.exp_mat_file)
+        tf_file = self.input_file(self.tf_names_file)
+        self.tf_names = utils.read_tf_names(tf_file)
+        self.meta_data = self.input_dataframe(self.meta_data_file)
+        self.priors_data = self.input_dataframe(self.priors_file)
+        self.gold_standard = self.input_dataframe(self.gold_standard_file)
+
+    def input_path(self, filename):
+        return os.path.abspath(os.path.join(self.input_dir, filename))
+
+    def input_file(self, filename, strict=True):
+        path = self.input_path(filename)
+        if os.path.exists(path):
+            return open(path)
+        elif not strict:
+            return None
+        raise ValueError("no such file " + repr(path))
+
+    def input_dataframe(self, filename, strict=True):
+        f = self.input_file(filename, strict)
+        if f is not None:
+            return utils.df_from_tsv(f)
+        else:
+            assert not strict
+            return None
+
+    def compute_common_data(self):
+        """
+        Compute common data structures like design and response matrices.
+        """
+        raise NotImplementedError  # implement in subclass
+
+    def get_priors(self):
+        """
+        Generate sequence of priors objects for run.
+        """
+        raise NotImplementedError  # implement in subclass
+
+    def get_bootstraps(self):
+        """
+        Generate sequence of bootstrap parameter objectss for run.
+        """
+        raise NotImplementedError  # implement in subclass
+
+    def emit_results(self, priors):
+        """
+        Output result report(s) for workflow run.
+        """
+        raise NotImplementedError  # implement in subclass
