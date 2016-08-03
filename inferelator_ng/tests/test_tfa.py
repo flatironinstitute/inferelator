938dcbe260ce87a74b12750838df63e6ca0e9bd5
Nick De Veaux <nrdeveaux@gmail.com>
2016-08-03 10:49:22 -0400

    refactored out the setup function so it can be easily changed. and added final combination of dup_self and no_self

--- inferelator_ng/tests/test_tfa.py
+++ inferelator_ng/tests/test_tfa.py
@@ -10,23 +10,27 @@ class TestTFA(unittest.TestCase):
 
     # Test for 3 genes, 2 of which are TFs, and 2 condidtions
     # where tau is equal to 1, so exp_mat and exp_mat_tau are equivalent
-    def test_two_by_two(self):
+    def setup(self):
         exp = pd.DataFrame(self.generate_random_matrix(2, 3))
         priors = pd.DataFrame(np.array([[0, 1, 0], [1, 0, 0]]).transpose())
-        tfa_class = tfa.TFA(priors, exp, exp)
-        activities = tfa_class.tfa()
+        self.tfa_class = tfa.TFA(priors, exp, exp)
+
+    def test_two_by_two(self):
+        self.setup()
+        activities = self.tfa_class.tfa()
         print activities
 
     def test_two_by_two_dupes(self):
-        exp = pd.DataFrame(self.generate_random_matrix(2, 3))
-        priors = pd.DataFrame(np.array([[0, 1, 0], [1, 0, 0]]))
-        tfa_class = tfa.TFA(priors, exp, exp)
-        activities = tfa_class.tfa(dup_self = False)
+        self.setup()
+        activities = self.tfa_class.tfa(dup_self = False)
         print activities
 
     def test_two_by_two_self_interactions(self):
-        exp = pd.DataFrame(self.generate_random_matrix(2, 3))
-        priors = pd.DataFrame(np.array([[0, 1, 0], [1, 0, 0]]))
-        tfa_class = tfa.TFA(priors, exp, exp)
-        activities = tfa_class.tfa(noself = False)
+        self.setup()
+        activities = self.tfa_class.tfa(noself = False)
+        print activities
+
+    def test_two_by_two_self_interactions_and_dupes(self):
+        self.setup()
+        activities = self.tfa_class.tfa(noself = False, dup_self = False)
         print activities
