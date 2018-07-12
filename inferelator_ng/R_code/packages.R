a658fb9aafc32136b8228c46b40f54182de0593b
CJackson <christopher@cajackson.org>
2018-07-12 16:50:34 -0400

    R is for REMOVED

--- inferelator_ng/R_code/packages.R
+++
@@ -1,4 +0,0 @@
-# commands to install needed R packages
-
-install.packages('inline', repos='http://cran.us.r-project.org')
-install.packages('parallel', repos='http://cran.us.r-project.org')
--- inferelator_ng/utils.py
+++ inferelator_ng/utils.py
@@ -44,51 +44,6 @@ class Debug:
     def notify(cls, *args, **kwargs):
         cls.vprint(*args, level=cls.levels["vv"], **kwargs)
 
-
-def convert_to_R_df(df):
-    """
-    Convert pandas dataframes so that they will be read correctly from CSV format by R.
-    """
-    new_df = pd.DataFrame(df)
-    # Convert booleans to "TRUE" and "FALSE" so they will be read correctly
-    for col in new_df.select_dtypes(include=[bool]):
-        new_df[col] = [str(x).upper() for x in new_df[col]]
-    # Replace null entries with NA entries
-    new_df.replace(r'\s+', 'NA', regex=True)
-    new_df.replace(np.nan, 'NA', regex=True)
-    return new_df
-
-
-def call_R(driver_path):
-    """
-    Run an "R" script in a subprocess.
-    Any outputs of the script, including stderr, should be saved to files.
-    """
-    r_command = ["R", "-f", driver_path]
-    try:
-        subprocess.check_output(r_command, stderr=subprocess.STDOUT, universal_newlines=True)
-        return
-    except subprocess.CalledProcessError as err:
-        print("R command {} failed to execute [Exit code {}]".format(err.cmd, err.returncode))
-        print(err.output)
-        raise
-
-
-
-    # if os.name == "posix":
-    #     command = "R -f " + driver_path
-    #     return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-    # else:
-    #     theproc = subprocess.Popen(['R', '-f', driver_path])
-    #     return theproc.communicate()
-
-
-def r_path(path):
-    """
-    Convert path to use conventions suitable for use in n "R" script.
-    """
-    return path.replace('\\', '/')
-
 def ownCheck(kvs, rank, chunk=1):
     """
     Generator
@@ -129,23 +84,6 @@ def kvsTearDown(kvs, rank):
     if 0 == rank:
         # Do a hard reset if rank == 0                                                                                                       
         kvs.get('count')
-
-class RDriver:
-    """
-    Superclass for R driver objects.
-    """
-    
-    target_directory = "/tmp"
-
-    def path(self, filename):
-        result = os.path.join(self.target_directory, filename).replace('\\', '/')
-        return r_path(result)
-
-
-def local_path(*location):
-    "Return location relative to the folder containing this module."
-    return r_path(os.path.join(my_dir, *location))
-
 
 def df_from_tsv(file_like, has_index = True):
     "Read a tsv file or buffer with headers and row ids into a pandas dataframe."
