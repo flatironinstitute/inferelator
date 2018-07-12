826b50d8b19bea053c4c92a1965df3d9a68626dc
CJackson <christopher@cajackson.org>
2018-07-12 15:03:20 -0400

    Refactored MI & CLR calculations into python

--- inferelator_ng/utils.py
+++ inferelator_ng/utils.py
@@ -2,12 +2,47 @@
 Miscellaneous utility modules.
 """
 
+from __future__ import print_function
 import os
 import pandas as pd
 import subprocess
 import numpy as np
 
 my_dir = os.path.dirname(__file__)
+
+
+class Debug:
+    verbose_level = 0
+    default_level = 1
+
+    levels = dict(normal=0,
+                  verbose=1, v=1,
+                  very_verbose=2, vv=2,
+                  max_output=3, vvv=3)
+
+    @classmethod
+    def set_verbose_level(cls, lvl):
+        if isinstance(lvl, (int, float)):
+            cls.verbose_level = lvl
+
+    @classmethod
+    def vprint(cls, *args, **kwargs):
+        try:
+            level = kwargs.pop('level')
+        except KeyError:
+            level = cls.default_level
+        if level <= cls.verbose_level:
+            print(*args, **kwargs)
+        else:
+            pass
+
+    @classmethod
+    def warn(cls, *args, **kwargs):
+        cls.vprint(*args, level=cls.levels["v"], **kwargs)
+
+    @classmethod
+    def notify(cls, *args, **kwargs):
+        cls.vprint(*args, level=cls.levels["vv"], **kwargs)
 
 
 def convert_to_R_df(df):
@@ -29,12 +64,23 @@ def call_R(driver_path):
     Run an "R" script in a subprocess.
     Any outputs of the script, including stderr, should be saved to files.
     """
-    if os.name == "posix":
-        command = "R -f " + driver_path
-        return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-    else:
-        theproc = subprocess.Popen(['R', '-f', driver_path])
-        return theproc.communicate()
+    r_command = ["R", "-f", driver_path]
+    try:
+        subprocess.check_output(r_command, stderr=subprocess.STDOUT, universal_newlines=True)
+        return
+    except subprocess.CalledProcessError as err:
+        print("R command {} failed to execute [Exit code {}]".format(err.cmd, err.returncode))
+        print(err.output)
+        raise
+
+
+
+    # if os.name == "posix":
+    #     command = "R -f " + driver_path
+    #     return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    # else:
+    #     theproc = subprocess.Popen(['R', '-f', driver_path])
+    #     return theproc.communicate()
 
 
 def r_path(path):
@@ -44,15 +90,37 @@ def r_path(path):
     return path.replace('\\', '/')
 
 def ownCheck(kvs, rank, chunk=1):
+    """
+    Generator
+
+    :param kvs: KVS object
+    :param rank: SLURM proc ID
+    :param chunk: The size of the chunk given to each subprocess
+
+    :yield: bool
+    """
     # initialize a global counter.                                                                                                               
+
+    #If we're the main process, set KVS count to 0
     if 0 == rank:
         kvs.put('count', 0)
+
+    #Start at the baseline
     checks, lower, upper = 0, -1, -1
+
     while 1:
+
+        #Checks increments every loop
+        #If it's greater than the upper bound, get a new lower bound from the KVS count
+        #Set the new upper bound by adding chunk to lower
+        #And then put the new upper bound back into KVS count
+
         if checks >= upper:
             lower = kvs.get('count')
             upper = lower + chunk
             kvs.put('count', upper)
+
+        #Yield TRUE if this row belongs to this process and FALSE if it doesn't
         yield lower <= checks < upper
         checks += 1
 
