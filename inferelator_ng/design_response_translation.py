89fd104d6a11956ac747e5702dcab1bccb7f96df
CJackson <christopher@cajackson.org>
2018-07-30 19:08:20 -0400

    Minor fixes & comments in DRD

--- inferelator_ng/design_response_translation.py
+++ inferelator_ng/design_response_translation.py
@@ -1,4 +1,3 @@
-import os
 from . import utils
 import pandas as pd
 import numpy as np
@@ -24,6 +23,7 @@ class PythonDRDriver:
 
     # Expression data
     exp_data = None
+    conds = None
 
     # Metadata
     meta_data = None
@@ -44,6 +44,12 @@ class PythonDRDriver:
         self.return_half_tau = return_half_tau
 
     def run(self, exp_data, meta_data):
+        """
+        Process expression data and metadata into design & response data
+        :param exp_data: pd.DataFrame [G x N]
+        :param meta_data: pd.DataFrame [N x 5]
+        :return design, response: pd.DataFrame [G x N], pd.DataFrame [G x N]
+        """
         (k, n) = exp_data.shape
         self.exp_data = exp_data
         self.meta_data = meta_data
@@ -65,32 +71,20 @@ class PythonDRDriver:
 
         # Walk through all the conditions in the expression data
         for c, cc in enumerate(self.conds):
-            utils.Debug.vprint("Processing condition {cc} [{c} / {tot}]".format(cc=cc, c=c, tot=n), level=3)
+            utils.Debug.vprint("Processing condition {cc} [{c} / {tot}]".format(cc=cc, c=c+1, tot=n), level=3)
             if self.steady_idx[cc]:
                 # This is a steady-state experiment
                 self.static_exp(c)
             else:
                 # This is a timecourse experiment - figure out what came before this timepoint
-                prev_cond, prev_delt = self.ts_group[cc][0]
+                prev_cond, prev_delt = self._get_prior_timepoint(cc)
                 if prev_cond is None:
-                    # Don't do anything for now, this is the first timepoint
+                    # This timepoint can't be put into this timecourse - move on
+                    # It will either be included later as the prior timepoint or it will end up as a steady-state
                     continue
-                elif self.delTmin <= prev_delt <= self.delTmax:
-                    # This is a valid timecourse experiment
-                    # Construct a design & response based on the prior timepoint
-                    self.timecourse_exp(c, self._get_index(prev_cond), prev_delt)
-                elif prev_delt > self.delTmax:
-                    # This is a timecourse experiment but the del.t is too big
-                    continue
-                elif prev_delt < self.delTmin:
-                    # This is a timecourse experiment but the del.t is too small
-                    # Try to find a prior timepoint that is acceptable
-                    back_cond, back_delt = self._walk_prior_timepoints(cc)
-                    if back_cond is not None:
-                        self.timecourse_exp(c, self._get_index(back_cond), back_delt)
                 else:
-                    continue
-
+                    # Insert timepoint data from this timecourse
+                    self.timecourse_exp(c, self._get_index(prev_cond), prev_delt)
 
         for c in np.where(~self.included)[0].tolist():
             # Run anything that wasn't included initially in as a steady-state experiment
@@ -119,7 +113,7 @@ class PythonDRDriver:
 
     def timecourse_exp(self, idx, prev_idx, prev_delt):
         """
-        Concatenate expression data onto design.
+        Concatenate expression data from the prior timepoint onto design.
         Calculate response data based on timecourse and concatenate the result onto response & response half.
         :param idx: int
         :param prev_idx: int
@@ -139,6 +133,13 @@ class PythonDRDriver:
         self.response_half = np.hstack((self.response_half, half_resp.reshape(-1, 1)))
 
     def _process_groups(self):
+        """
+        Parse the metadata to identify steady-state experiments and link timecourse experiments
+        :return steady_idx, ts_group: dict, dict
+            steady_idx:  Dict keyed by condition, value is boolean (True if the condition is a steady-state experiment)
+            ts_group: Dict keyed by condition. [(Previous_condition_name, Previous_delt),
+                                                (Following_condition, Following_delt)]
+        """
         time_series = self.meta_data[TS_col_name].values.astype(bool)
         steadies = dict(zip(self.meta_data[CONDITION_col_name], np.logical_not(time_series)))
 
@@ -166,6 +167,10 @@ class PythonDRDriver:
         self.ts_group = ts_group
 
     def _check_for_dupes(self):
+        """
+        Sanity check the metadata and experimental data to ensure that they can be linked. Try to guess as much as
+        possible unless strict checking is on
+        """
         # Check to make sure that the conditions in the expression data are matched with conditions in the metadata
         exp_conds = self.exp_data.columns
         meta_conds = self.meta_data[CONDITION_col_name]
@@ -178,19 +183,18 @@ class PythonDRDriver:
             utils.Debug.vprint("The following conditions cannot be properly matched to metadata:", level=1)
             utils.Debug.vprint(" ".join(in_exp_not_meta), level=1)
             if self.strict_checking_for_metadata:
-                raise ValueError("Conditions exist without associated metadata")
+                raise ConditionDoesNotExistError("Conditions exist without associated metadata")
             else:
                 for condition in in_exp_not_meta:
                     self.steady_idx[condition] = True
 
         # Check to find out if the conditions in the expression data are all unique
         # It's not a problem if they're not, just assume the metadata applies to all conditions with the same name
-
         duplicate_in_exp = self.exp_data.columns.duplicated()
         if np.sum(duplicate_in_exp) > 0:
             c_dup = self.exp_data.columns[duplicate_in_exp].tolist()
-            utils.Debug.vprint("The expression data has non-unique condition indexes: {}".format(" ".join(c_dup)),
-                               level=1)
+            utils.Debug.vprint("The expression data has non-unique condition indexes:", level=1)
+            utils.Debug.vprint(" ".join(c_dup), level=1)
 
         # Check to find out if the conditions in the meta data are all unique
         # If there are repeats, check and see if the associated information is identical (if it is, just ignore it)
@@ -202,17 +206,22 @@ class PythonDRDriver:
                 utils.Debug.vprint("The metadata has non-unique conditions with different characteristics:", level=0)
                 utils.Debug.vprint(" ".join(meta_dup), level=0)
                 if self.strict_checking_for_duplicates:
-                    raise ValueError("Multiple conditions exist in the metadata with different characteristics")
+                    raise MultipleConditionsError("Identical conditions have non-identical characteristics")
             else:
                 utils.Debug.vprint("The metadata contains duplicate rows: {}".format(" ".join(meta_dup)), level=1)
 
-    def _walk_prior_timepoints(self, cond):
+    def _get_prior_timepoint(self, cond):
+        """
+        Walk backwards through timepoints until a total del.t that falls within the acceptable window is located
+        :param cond:
+        :return:
+        """
         total_delt = 0
-        for pc, pd in self._prior_timepoint_generator(cond):
-            total_delt += pd
+        for pcond, pdelt in self._prior_timepoint_generator(cond):
+            total_delt += pdelt
             if self.delTmin <= total_delt <= self.delTmax:
-                return pc, total_delt
-            elif total_delt> self.delTmax:
+                return pcond, total_delt
+            elif total_delt > self.delTmax:
                 return None, None
         return None, None
 
@@ -227,6 +236,12 @@ class PythonDRDriver:
             prev_cond, prev_delt = self.ts_group[prev_cond][0]
 
     def _get_index(self, cond):
+        """
+        Look up the index in the expression data of a specific condition. Raise errors if it doesn't exist, or if it's
+        not unique
+        :param cond:
+        :return idx: int
+        """
         idx = np.where(self.conds == cond)[0].tolist()
         if len(idx) == 0:
             raise ConditionDoesNotExistError("{cond} cannot be identified in expression conditions".format(cond=cond))
@@ -239,135 +254,10 @@ class PythonDRDriver:
     def _fix_NAs(self):
         self.meta_data = self.meta_data.replace('NA', np.nan, regex=False)
 
+
 class ConditionDoesNotExistError(IndexError):
     pass
 
+
 class MultipleConditionsError(ValueError):
     pass
-
-class PythonDRDriver_old:
-
-    def __init__(self):
-        pass
-
-    def run(self, expression_mat, metadata_dataframe):
-
-        meta_data = metadata_dataframe.copy()
-        meta_data = meta_data.replace('NA', np.nan, regex=False)
-        exp_mat = expression_mat.copy()
-
-        special_char_dictionary = {'+': 'specialplus', '-': 'specialminus', '.': 'specialperiod', '/': 'specialslash',
-                                   '\\': 'special_back_slash', ')': 'special_paren_backward',
-                                   '(': 'special_paren_forward', ',': 'special_comma', ':': 'special_colon',
-                                   ';': 'special_semicoloon', '@': 'special_at', '=': 'special_equal',
-                                   '>': 'special_great', '<': 'special_less', '[': 'special_left_bracket',
-                                   ']': 'special_right_bracket', "%": 'special_percent', "*": 'special_star',
-                                   '&': 'special_ampersand', '^': 'special_arrow', '?': 'special_question',
-                                   '!': 'special_exclamation', '#': 'special_hashtag', "{": 'special_left_curly',
-                                   '}': 'special_right_curly', '~': 'special_tilde', '`': 'special_tildesib',
-                                   '$': 'special_dollar', '|': 'special_vert_bar'}
-
-        cols = exp_mat.columns.tolist()
-        for ch in special_char_dictionary.keys():
-            # need this edge case for passing micro test
-            if len(meta_data['condName'][~meta_data['condName'].isnull()]) > 0:
-                meta_data['condName'] = meta_data['condName'].str.replace(ch, special_char_dictionary[ch])
-            if len(meta_data['prevCol'][~meta_data['prevCol'].isnull()]) > 0:
-                meta_data['prevCol'] = meta_data['prevCol'].str.replace(ch, special_char_dictionary[ch])
-            cols = [item.replace(ch, special_char_dictionary[ch]) for item in cols]
-        exp_mat.columns = cols
-
-        cond = meta_data['condName'].copy()
-        prev = meta_data['prevCol'].copy()
-        delt = meta_data['del.t'].copy()
-        delTmin = self.delTmin
-        delTmax = self.delTmax
-        tau = self.tau
-        prev.loc[delt > delTmax] = np.nan
-        delt.loc[delt > delTmax] = np.nan
-        not_in_mat = set(cond) - set(exp_mat)
-        cond_dup = cond.duplicated()
-        if len(not_in_mat) > 0:
-            cond = cond.str.replace('[/+-]', '.')
-            prev = cond.str.replace('[/+-]', '.')
-            if cond_dup != cond.duplicated():
-                raise ValueError(
-                    'Tried to fix condition names in meta data so that they would match column names in expression matrix, but failed')
-
-        # check if there are condition names missing in expression matrix
-        not_in_mat = set(cond) - set(exp_mat)
-        if len(not_in_mat) > 0:
-            print(not_in_mat)
-            raise ValueError(
-                'Error when creating design and response. The conditions printed above are in the meta data, but not in the expression matrix')
-
-        cond_n_na = cond[~cond.isnull()]
-        steady = prev.isnull() & ~(cond_n_na.isin(prev.replace(np.nan, "NA")))
-
-        des_mat = pd.DataFrame(exp_mat[cond[steady]])
-        res_mat = pd.DataFrame(exp_mat[cond[steady]])
-
-        for i in list(np.where(~steady)[0]):
-            following = list(np.where(prev.str.contains(cond[i]) == True)[0])
-            following_delt = list(delt[following])
-
-            try:
-                off = list(np.where(following_delt[0] < delTmin)[0])
-            except:
-                off = []
-
-            while len(off) > 0:
-                off_fol = list(np.where(prev.str.contains(cond[following[off[0]]]) == True)[0])
-                off_fol_delt = list(delt[off_fol])
-                following = following[:off[0]] + following[off[0] + 1:] + off_fol
-                following_delt = following_delt[:off[0]] + following_delt[off[0] + 1:] + [
-                    float(off_fol_delt[0]) + float(following_delt[off[0]])]
-                off = list(np.where(following_delt < [delTmin])[0])
-
-            n = len(following)
-            cntr = 0
-
-            for j in following:
-                if n > 1:
-                    this_cond = "%s_dupl%02d" % (cond[i], cntr + 1)
-                    original_this_cond = this_cond
-                    k = 1
-                    while this_cond in res_mat.columns:
-                        this_cond = original_this_cond + '.{}'.format(int(k))
-                        k = k + 1
-                else:
-                    this_cond = cond[i]
-
-                des_tmp = np.concatenate((des_mat.values, exp_mat[cond[i]].values[:, np.newaxis]), axis=1)
-                des_names = list(des_mat.columns) + [this_cond]
-                des_mat = pd.DataFrame(des_tmp, index=des_mat.index, columns=des_names)
-                interp_res = (float(tau) / float(following_delt[cntr])) * (
-                            exp_mat[cond[j]].astype('float64') - exp_mat[cond[i]].astype('float64')) + exp_mat[
-                                 cond[i]].astype('float64')
-                res_tmp = np.concatenate((res_mat.values, interp_res.values[:, np.newaxis]), axis=1)
-                res_names = list(res_mat.columns) + [this_cond]
-                res_mat = pd.DataFrame(res_tmp, index=res_mat.index, columns=res_names)
-
-                cntr = cntr + 1
-
-            # special case: nothing is following this condition within delT.min
-            # and it is the first of a time series --- treat as steady state
-
-            if n == 0 and prev.isnull()[i]:
-                des_mat = pd.concat([des_mat, exp_mat[cond[i]]], axis=1)
-                des_mat.rename(columns={des_mat.columns.values[len(des_mat.columns) - 1]: cond[i]}, inplace=True)
-                res_mat = pd.concat([res_mat, exp_mat[cond[i]]], axis=1)
-                res_mat.rename(columns={res_mat.columns.values[len(res_mat.columns) - 1]: cond[i]}, inplace=True)
-
-        cols_des_mat = des_mat.columns.tolist()
-        cols_res_mat = res_mat.columns.tolist()
-
-        special_char_inv_map = {v: k for k, v in list(special_char_dictionary.items())}
-        for sch in special_char_inv_map.keys():
-            cols_des_mat = [item.replace(sch, special_char_inv_map[sch]) for item in cols_des_mat]
-            cols_res_mat = [item.replace(sch, special_char_inv_map[sch]) for item in cols_res_mat]
-
-        des_mat.columns = cols_des_mat
-        res_mat.columns = cols_res_mat
-
-        return (des_mat, res_mat)
