0021a32775d0e816d1136f56d9c1feba54616913
CJackson <christopher@cajackson.org>
2018-08-28 15:58:03 -0400

    Import optimization

--- inferelator_ng/single_cell_bbsr_tfa_workflow.py
+++ inferelator_ng/single_cell_bbsr_tfa_workflow.py
@@ -77,14 +77,12 @@ class Single_Cell_BBSR_TFA_Workflow(bbsr_tfa_workflow.BBSR_TFA_Workflow):
         self.expression_matrix = np.zeros((len(idx), len(cols)), dtype=dtype)
 
         st = time.time()
-        utils.Debug.vprint("Reading {f} file data".format(f=file_name), end="")
+        utils.Debug.vprint("Reading {f} file data".format(f=file_name))
         for i, df in enumerate(pd.read_csv(file_name, sep="\t", header=None, skiprows=1, usecols=range(1,len(cols)+1),
-                                           dtype=dtype, iterator=True)):
+                                           dtype=dtype, chunksize=1, iterator=True)):
             self.expression_matrix[i,:] = df.values
             if i % 100:
-                utils.Debug.vprint(".", end="")
-        utils.Debug.vprint("Done")
-
+                utils.Debug.vprint("[{n}/{t}".format(n=i, t=len(idx)), level=2)
         et = int(time.time() - st)
 
         self.expression_matrix = pd.DataFrame(self.expression_matrix, index=idx, columns=cols)
