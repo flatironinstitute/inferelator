08ab542a1e72822619b67d3c79c13cdc2e7f0042
CJackson <christopher@cajackson.org>
2018-11-15 11:39:25 -0500

    Flatten betas

--- inferelator_ng/elasticnet_python.py
+++ inferelator_ng/elasticnet_python.py
@@ -26,33 +26,53 @@ MIN_COEF = 0.1
 
 
 def elastic_net(X, Y, params):
-    (k, n) = X.shape
-    X = X.T
+    """
+
+    :param X: np.ndarray [K x N]
+    :param Y: np.ndarray [1 x N]
+    :param params: dict
+    :return:
+    """
+    (K, N) = X.shape
+    X = X.T             # Make X into [N, K]
+    Y = Y.flatten()     # Make Y into [N, ]
+
     # Fit the linear model using the elastic net
-    model = ElasticNetCV(**params).fit(X, Y.ravel())
+    model = ElasticNetCV(**params).fit(X, Y)
 
     # Set coefficients below threshold to 0
-    model.coef_[np.abs(model.coef_) < MIN_COEF] = 0.
-    coef_nonzero = model.coef_ != 0
+    coefs = model.coef_                     # Get all model coefficients [K, ]
+    coefs[np.abs(coefs) < MIN_COEF] = 0.    # Threshold coefficients
+    coef_nonzero = coefs != 0               # Create a boolean array where coefficients are nonzero [K, ]
 
+    # If there are non-zero coefficients, redo the linear regression with them alone
+    # And calculate beta_resc
     if coef_nonzero.sum() > 0:
-        idx = utils.bool_to_index(coef_nonzero)
-        x = X[:, idx]
-        best_betas = np.linalg.solve(np.dot(x.T, x), np.dot(x.T, Y))
-        betas_resc = predict_error_reduction(x, Y, best_betas)
+        x = X[:, coef_nonzero] # Get only the predictors (k) which elasticnet likes [N, k]
+        utils.make_array_2d(Y) # Make the output 2d [N, 1]
+        best_betas = np.linalg.solve(np.dot(x.T, x), np.dot(x.T, Y)).flatten()   # Get betas from regression [k, ]
+        betas_resc = predict_error_reduction(x, Y, best_betas)                   # Calculate beta_resc [k, ]
         return dict(pp=coef_nonzero,
                     betas=best_betas,
                     betas_resc=betas_resc)
     else:
-        return dict(pp=np.repeat(True, k).tolist(),
-                    betas=np.zeros(k),
-                    betas_resc=np.zeros(k))
+        return dict(pp=np.repeat(True, K).tolist(),
+                    betas=np.zeros(K),
+                    betas_resc=np.zeros(K))
 
 
 class ElasticNet(regression.BaseRegression):
     params = ELASTICNET_PARAMETERS
 
     def __init__(self, X, Y, kvs, chunk=25):
+        """
+        Create a ElasticNet object for regularized regression
+
+        :param X: pd.DataFrame [K x N]
+            Expression / Activity data
+        :param Y: pd.DataFrame [G x N]
+            Response data
+        """
         self.kvs = kvs
         self.chunk = chunk
 
@@ -70,7 +90,7 @@ class ElasticNet(regression.BaseRegression):
 
     def run(self):
         """
-        Execute BBSR separately on each response variable in the data
+        Execute regression separately on each response variable in the data
 
         :return: pd.DataFrame [G x K], pd.DataFrame [G x K]
             Returns the regression betas and beta error reductions for all threads if this is the master thread (rank 0)
