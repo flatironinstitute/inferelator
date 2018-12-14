73f9533939742792281b2ffae3ec4c6ae853fa12
CJackson <christopher@cajackson.org>
2018-12-14 11:06:50 -0500

    Added state check to sync_process

--- inferelator_ng/kvs_controller.py
+++ inferelator_ng/kvs_controller.py
@@ -8,6 +8,7 @@ It also keeps track of a bunch of SLURM related stuff that was previously workfl
 from kvsstcp import KVSClient
 
 import os
+import warnings
 
 # SLURM environment variables
 SBATCH_VARS = dict(SLURM_PROCID=('rank', int, 0),
@@ -69,23 +70,36 @@ class KVSController(KVSClient):
         if self.is_master:
             self.get(kvs_key)
 
-    def sync_processes(self, pref=""):
-        # Block all processes until they reach this point
-        # Then release them
-        # It may be wise to use unique prefixes if this is gonna get called rapidly so there's no collision
-        # Or not. I'm a comment, not a cop.
+    def sync_processes(self, pref="", value=True):
+        """
+        Block all processes until they reach this point, then release them
+        It may be wise to use unique prefixes if this is gonna get called rapidly so there's no collision
+        Or not. I'm a comment, not a cop.
+
+        :param pref: str
+            Prefix attached to the KVS keys
+        :param value: Anything you can pickle
+            A value that will be checked for consistency between processes (if you set a different value in a
+            process, a warning will be issued. This is mostly to check state if needed
+        :return None:
+        """
+
 
         wkey = pref + '_wait'
         ckey = pref + '_continue'
 
         # Every process puts a wait key up when it gets here
-        self.put(wkey, True)
+        self.put(wkey, value)
 
         # The master pulls down the wait keys until it has all of them
         # Then it puts up a go key for each process
+
         if self.is_master:
             for _ in range(self.tasks):
-                self.get(wkey)
+                c_value = self.get(wkey)
+                if c_value != value:
+                    warnings.warn("Sync warning: master {val_m} is not equal to client {val_c}".format(val_m=value,
+                                                                                                       val_c=c_value))
             for _ in range(self.tasks):
                 self.put(ckey, True)
 
