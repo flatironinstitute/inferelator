699e2a9f8748015ba25812052fe370214909982c
CJackson <christopher@cajackson.org>
2019-01-22 15:39:34 -0500

    Changed RP passing and added comments

--- inferelator_ng/amusr_workflow.py
+++ inferelator_ng/amusr_workflow.py
@@ -22,19 +22,62 @@ from inferelator_ng import results_processor
 class ResultsProcessorMultiTask(results_processor.ResultsProcessor):
     """
     This results processor should handle the results of the MultiTask inferelator
+
+    It will output the results for each task, as well as rank-combining to construct a network from all tasks
     """
 
     write_task_files = True
+    tasks_names = []
+
+    def __init__(self, betas, rescaled_betas, threshold=0.5, filter_method='overlap', tasks_names=None):
+        """
+        :param betas: list(pd.DataFrame[G x K])
+        :param rescaled_betas: list(pd.DataFrame[G x K])
+        :param threshold: float
+        :param filter_method: str
+            How to handle gold standard filtering ('overlap' filters to beta, 'keep_all_gold_standard' doesn't filter)
+        :param tasks_names: list(str)
+            The names for each task
+        """
+        self.betas = betas
+        self.rescaled_betas = rescaled_betas
+        self.filter_method = filter_method
+
+        if 1 >= threshold >= 0:
+            self.threshold = threshold
+        else:
+            raise ValueError("Threshold must be a float in the interval [0, 1]")
+
+        if tasks_names is not None:
+            self.tasks_names = tasks_names
 
     def summarize_network(self, output_dir, gold_standard, priors, confidence_threshold=default.DEFAULT_CONF,
                           precision_threshold=default.DEFAULT_PREC):
+        """
+        Take the betas and rescaled beta_errors, construct a network, and test it against the gold standard
+        :param output_dir: str
+            Path to write files into. Don't write anything if this is None.
+        :param gold_standard: pd.DataFrame [G x K]
+            Gold standard to test the network against
+        :param priors: pd.DataFrame [G x K]
+            Prior data
+        :param confidence_threshold: float
+        :param precision_threshold: float
+        :return aupr: float
+            Returns the AUPR calculated from the network and gold standard
+        :return stable_interactions: int
+            Number of interactions with a combined confidence over confidence_threshold
+        :return precision_interactions: int
+            Number of interactions with a combined confidence over the precision from precision_threshold
+        """
+
         overall_confidences = []
         overall_resc_betas = []
         overall_sign = pd.DataFrame(np.zeros(self.betas[0][0].shape), index=self.betas[0][0].index,
                                     columns=self.betas[0][0].columns)
         overall_threshold = overall_sign.copy()
 
-        for task_id, task_dir in enumerate(output_dir[1]):
+        for task_id, task_dir in enumerate(self.tasks_names):
             pr_calc = results_processor.RankSummaryPR(self.rescaled_betas[task_id], gold_standard,
                                                       filter_method=self.filter_method)
             task_threshold, task_sign, task_nonzero = self.threshold_and_summarize(self.betas[task_id], self.threshold)
@@ -50,7 +93,7 @@ class ResultsProcessorMultiTask(results_processor.ResultsProcessor):
             utils.Debug.vprint("Model AUPR:\t{aupr}".format(aupr=pr_calc.aupr), level=0)
 
             if self.write_task_files is True and output_dir is not None:
-                self.write_output_files(pr_calc, os.path.join(output_dir[0], task_dir), priors, task_threshold,
+                self.write_output_files(pr_calc, os.path.join(output_dir, task_dir), priors, task_threshold,
                                         network_data)
 
         overall_pr_calc = results_processor.RankSummaryPR(overall_confidences, gold_standard,
@@ -62,7 +105,7 @@ class ResultsProcessorMultiTask(results_processor.ResultsProcessor):
 
         utils.Debug.vprint("Model AUPR:\t{aupr}".format(aupr=overall_pr_calc.aupr), level=0)
 
-        self.write_output_files(overall_pr_calc, output_dir[0], priors, overall_threshold, network_data)
+        self.write_output_files(overall_pr_calc, output_dir, priors, overall_threshold, network_data)
 
         # Calculate how many interactions are stable (are above the combined confidence threshold)
         stable_interactions = overall_pr_calc.num_over_conf_threshold(confidence_threshold)
@@ -73,6 +116,12 @@ class ResultsProcessorMultiTask(results_processor.ResultsProcessor):
 
 
 class SingleCellMultiTask(single_cell_workflow.SingleCellWorkflow, single_cell_puppeteer_workflow.PuppeteerWorkflow):
+    """
+    Class that implements AMuSR multitask learning for single cell data
+
+    Extends SingleCellWorkflow
+    Inherits from PuppeteerWorkflow so that task preprocessing can be done more easily
+    """
     regression_type = amusr_regression
     prior_weight = 1.
     task_expression_filter = "intersection"
@@ -105,10 +154,12 @@ class SingleCellMultiTask(single_cell_workflow.SingleCellWorkflow, single_cell_p
     def separate_tasks_by_metadata(self, meta_data_column=default.DEFAULT_METADATA_FOR_BATCH_CORRECTION):
         """
         Take a single expression matrix and break it into multiple dataframes based on meta_data. Reset the
-        self.expression_matrix and self.meta_data with a list of dataframes
+        self.expression_matrix and self.meta_data with a list of dataframes, self.n_tasks with the number of tasks,
+        and self.tasks_names with the name from meta_data for each task.
 
         :param meta_data_column: str
             Meta_data column which corresponds to task ID
+
         """
 
         task_name, task_data, task_metadata = [], [], []
@@ -128,7 +179,12 @@ class SingleCellMultiTask(single_cell_workflow.SingleCellWorkflow, single_cell_p
 
     def process_task_data(self):
         """
-        Preprocess the individual task data using a child worker into task design and response data
+        Preprocess the individual task data using a child worker into task design and response data. Set
+        self.task_design, self.task_response, self.task_meta_data, self.task_bootstraps with lists which contain
+        DataFrames.
+
+        Also set self.regulators and self.targets with pd.Indexes that correspond to the genes and tfs to model
+        This is chosen based on the filtering strategy set in self.task_expression_filter
         """
 
         self.task_design, self.task_response, self.task_meta_data, self.task_bootstraps = [], [], [], []
@@ -158,8 +214,9 @@ class SingleCellMultiTask(single_cell_workflow.SingleCellWorkflow, single_cell_p
         """
         if self.is_master():
             self.create_output_dir()
-            rp = ResultsProcessorMultiTask(betas, rescaled_betas, filter_method=self.gold_standard_filter_method)
-            results = rp.summarize_network((self.output_dir, self.tasks_names), gold_standard, priors_data)
+            rp = ResultsProcessorMultiTask(betas, rescaled_betas, filter_method=self.gold_standard_filter_method,
+                                           tasks_names=self.tasks_names)
+            results = rp.summarize_network(self.output_dir, gold_standard, priors_data)
             self.aupr, self.n_interact, self.precision_interact = results
         else:
             self.aupr, self.n_interact, self.precision_interact = None, None, None
