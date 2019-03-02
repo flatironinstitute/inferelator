74fb31fb07d514301fba124e844804362eccef8f
CJackson <christopher@cajackson.org>
2019-03-02 14:36:55 -0500

    Local single-process controller

--- inferelator_ng/distributed/local_controller.py
+++ inferelator_ng/distributed/local_controller.py
@@ -2,14 +2,16 @@
 LocalController just runs everything in a single process
 """
 
-import itertools
+import collections
 
-from inferelator_ng.distributed import AbstractController, process_dask_graph, process_dask_function_args
+from inferelator_ng.distributed import AbstractController
+from inferelator_ng.utils import Validator as check
 
 
 class LocalController(AbstractController):
     client = None
     is_master = True
+    chunk = None
 
     @classmethod
     def connect(cls, *args, **kwargs):
@@ -20,26 +22,7 @@ class LocalController(AbstractController):
         return True
 
     @classmethod
-    def get(cls, dsk, result, *args, **kwargs):
-        func, map_args, iter_args, iter_product = process_dask_graph(dsk, result)
-
-        if map_args is None:
-            return func()
-        elif iter_args is None:
-            return func(*map_args)
-
-        # Just chew through the results all in order
-        results = {pos: func(*process_dask_function_args(map_args, iter_args, iterated_args))
-                   for pos, iterated_args in enumerate(itertools.product(*iter_product))}
-
-        # Process results
-        return cls.process_results(results)
-
-    @classmethod
-    def process_results(cls, results):
-        # Put everything into a list based on the dict key
-        pileup_list = [None] * len(results)
-        for idx, val in results.items():
-            pileup_list[idx] = val
-
-        return pileup_list
+    def map(cls, func, arg, **kwargs):
+        assert check.argument_callable(func)
+        assert check.argument_type(arg, collections.Iterable)
+        return list(map(func, arg))
