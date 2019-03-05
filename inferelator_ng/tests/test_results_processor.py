df204fb12749b73af4b98ca9d040707dcd5d7322
Claudia Skok Gibbs <cskokgibbs@ccbmac072.flatironinstitute.org>
2019-03-05 12:29:02 -0500

    pr_curve_test4

--- inferelator_ng/tests/test_results_processor.py
+++ inferelator_ng/tests/test_results_processor.py
@@ -186,6 +186,6 @@ class TestResultsProcessor(unittest.TestCase):
         self.assertTrue(exists)
 
     def test_plot_pr_curve_file_name(self):
-        results_processor.RankSummaryPR.plot_pr_curve(file_name = None)
-        results_processor.RankSummaryPR.plot_pr_curve(output_dir = None)
-        results_processor.RankSummaryPR.plot_pr_curve(recall = 0.7, precision = 0.5)
+        results_processor.RankSummaryPR.plot_pr_curve(file_name=None)
+        results_processor.RankSummaryPR.plot_pr_curve(output_dir=None)
+        results_processor.RankSummaryPR.plot_pr_curve(recall=0.7, precision=0.5)
