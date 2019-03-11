08c8beab51ebaa7b23aeecc19dd44ad7dec274e0
CJackson <christopher@cajackson.org>
2019-03-11 11:14:58 -0400

    Changed logic for task scatter

--- inferelator_ng/regression/amusr_regression.py
+++ inferelator_ng/regression/amusr_regression.py
@@ -528,7 +528,7 @@ def regress_dask(X, Y, priors, prior_weight, n_tasks, genes, tfs, G, remove_auto
     """
     from inferelator_ng.distributed.dask_controller import DaskController
 
-    def regression_maker(j, x_df, y_dict, prior, tf):
+    def regression_maker(j, x_df, y_list, prior, tf):
         level = 0 if j % 100 == 0 else 2
         utils.Debug.allprint(base_regression.PROGRESS_STR.format(gn=genes[j], i=j, total=G),
                              level=level)
@@ -541,21 +541,21 @@ def regress_dask(X, Y, priors, prior_weight, n_tasks, genes, tfs, G, remove_auto
         else:
             pass
 
-        for k in y_dict.keys():
+        for k, y_data in y_list:
             x.append(x_df[k].loc[:, tf].values)  # list([N, K])
-            y.append(y_dict[k])
+            y.append(y_data)
             tasks.append(k)  # [T,]
 
-        del y_dict
+        del y_list
         prior = format_prior(prior, gene, tasks, prior_weight)
         return run_regression_EBIC(x, y, tfs, tasks, gene, prior)
 
     def response_maker(y_df, i):
-        y = dict()
+        y = []
         gene = genes[i]
         for k in range(n_tasks):
             if gene in y_df[k]:
-                y[i] = y_df[k].loc[:, gene].values.reshape(-1, 1)
+                y.append((k, y_df[k].loc[:, gene].values.reshape(-1, 1)))
         return y
 
     [scatter_x] = DaskController.client.scatter([X], broadcast=True)
