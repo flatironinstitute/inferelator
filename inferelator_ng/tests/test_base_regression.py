210c88abc8837112e422d9d90d3d97b6a41f0358
Claudia Skok Gibbs <cskokgibbs@ccbmac072.flatironinstitute.org>
2019-03-12 16:22:45 -0400

    base

--- inferelator_ng/tests/test_base_regression.py
+++ inferelator_ng/tests/test_base_regression.py
@@ -10,3 +10,8 @@ class TestBaseRegression(unittest.TestCase):
         df = pd.DataFrame(np.array([[0, 1], [0, 1]]))
         result = base_regression.BaseRegression._scale(df)
         np.testing.assert_array_almost_equal(result, np.array([[-0.707107, 0.707107], [-0.707107, 0.707107]]), 5)
+
+    def test_recalculate_betas_from_selected(self):
+        x = np.array([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]])
+        y = np.array([0, 1, 0, 1, 0])
+        base_regression.recalculate_betas_from_selected(x, y, idx=None)
