1329886402c21e095c80173167a58c8c07839a8d
Claudia Skok Gibbs <cskokgibbs@ccbmac072.flatironinstitute.org>
2019-03-12 16:46:09 -0400

    base regress

--- inferelator_ng/tests/test_base_regression.py
+++ inferelator_ng/tests/test_base_regression.py
@@ -14,4 +14,10 @@ class TestBaseRegression(unittest.TestCase):
     def test_recalculate_betas_from_selected(self):
         x = np.array([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]])
         y = np.array([0, 1, 0, 1, 0])
-        base_regression.recalculate_betas_from_selected(x, y, idx=None)
+        result = base_regression.recalculate_betas_from_selected(x, y, idx=None)
+        np.testing.assert_array_almost_equal(result, np.array([-0.4, 0.4]))
+
+    def test_bool_to_index(self):
+        arr = np.array([[0, 1], [1, 0]])
+        result = base_regression.bool_to_index(arr)
+        np.testing.assert_array_almost_equal(result, np.array([0, 1]))
