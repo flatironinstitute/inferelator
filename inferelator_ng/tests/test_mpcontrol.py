dbd7573eaf85670ca3e87f846c344056b679491b
CJackson <christopher@cajackson.org>
2019-03-25 17:10:13 -0400

    More tests for error states

--- inferelator_ng/tests/test_mpcontrol.py
+++ inferelator_ng/tests/test_mpcontrol.py
@@ -51,6 +51,38 @@ class TestMPControl(unittest.TestCase):
         MPControl.connect()
 
 
+class TestNoController(TestMPControl):
+    @classmethod
+    def setUpClass(cls):
+        MPControl.shutdown()
+
+    @classmethod
+    def tearDownClass(cls):
+        MPControl.shutdown()
+        MPControl.set_multiprocess_engine("local")
+        MPControl.connect()
+
+    def test_map(self):
+        with self.assertRaises(ConnectionError):
+            MPControl.map(math_function, *self.map_test_data)
+
+    def test_sync(self):
+        with self.assertRaises(ConnectionError):
+            MPControl.sync_processes()
+
+    def test_name(self):
+        with self.assertRaises(NameError):
+            MPControl.name()
+
+    def test_bad_engine(self):
+        with self.assertRaises(ValueError):
+            MPControl.set_multiprocess_engine("V8")
+
+    def test_bad_engine_II(self):
+        with self.assertRaises(ValueError):
+            MPControl.set_multiprocess_engine(unittest.TestCase)
+
+
 class TestLocalController(TestMPControl):
     name = "local"
 
@@ -94,7 +126,7 @@ class TestKVSMPController(TestMPControl):
         self.assertListEqual(test_result, self.map_test_expect)
 
     def test_kvs_sync(self):
-        MPControl.sync_processes()
+        self.assertEqual(MPControl.sync_processes(), None)
 
     def test_kvs_name(self):
         self.assertEqual(MPControl.name(), self.name)
@@ -118,37 +150,38 @@ class TestMultiprocessingMPController(TestMPControl):
         self.assertTrue(MPControl.sync_processes())
 
 
-@unittest.skipIf(not TEST_DASK, "Dask not installed")
-class TestDaskLocalMPController(TestMPControl):
-    name = "dask-local"
-    client_name = "dask"
-    tempdir = None
+if __name__ == '__main__':
+    @unittest.skipIf(not TEST_DASK, "Dask not installed")
+    class TestDaskLocalMPController(TestMPControl):
+        name = "dask-local"
+        client_name = "dask"
+        tempdir = None
 
-    @classmethod
-    def setUpClass(cls):
-        cls.tempdir = tempfile.mkdtemp()
-        MPControl.shutdown()
-        MPControl.set_multiprocess_engine(cls.name)
-        MPControl.connect(local_dir=cls.tempdir, n_workers=1)
+        @classmethod
+        def setUpClass(cls):
+            cls.tempdir = tempfile.mkdtemp()
+            MPControl.shutdown()
+            MPControl.set_multiprocess_engine(cls.name)
+            MPControl.connect(local_dir=cls.tempdir, n_workers=1)
 
-    @classmethod
-    def tearDownClass(cls):
-        super(TestDaskLocalMPController, cls).tearDownClass()
-        if cls.tempdir is not None:
-            shutil.rmtree(cls.tempdir)
+        @classmethod
+        def tearDownClass(cls):
+            super(TestDaskLocalMPController, cls).tearDownClass()
+            if cls.tempdir is not None:
+                shutil.rmtree(cls.tempdir)
 
-    def test_dask_local_connect(self):
-        self.assertTrue(MPControl.is_initialized)
+        def test_dask_local_connect(self):
+            self.assertTrue(MPControl.is_initialized)
 
-    def test_dask_local_name(self):
-        self.assertEqual(MPControl.name(), self.client_name)
+        def test_dask_local_name(self):
+            self.assertEqual(MPControl.name(), self.client_name)
 
-    def test_dask_local_map(self):
-        with self.assertRaises(NotImplementedError):
-            MPControl.map(math_function, *self.map_test_data)
+        def test_dask_local_map(self):
+            with self.assertRaises(NotImplementedError):
+                MPControl.map(math_function, *self.map_test_data)
 
-    def test_dask_local_sync(self):
-        self.assertTrue(MPControl.sync_processes())
+        def test_dask_local_sync(self):
+            self.assertTrue(MPControl.sync_processes())
 
-    def test_dask_local_workers_running(self):
-        self.assertTrue(os.path.isfile(os.path.join(self.tempdir, "global.lock")))
+        def test_dask_local_workers_running(self):
+            self.assertTrue(os.path.isfile(os.path.join(self.tempdir, "global.lock")))
