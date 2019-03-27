4d2627e5abb1a0899ed69843af8cca48ec3ef936
CJackson <christopher@cajackson.org>
2019-03-27 15:59:14 -0400

    More workflow tests

--- inferelator_ng/tests/test_workflow_stub.py
+++ inferelator_ng/tests/test_workflow_stub.py
@@ -1,10 +1,10 @@
 """
-Test workflow logic outline using completely
-artificial stubs for dependancies.
+Test base workflow stepwise.
 """
 
 import unittest
 import os
+import tempfile
 import numpy as np
 
 from inferelator_ng import workflow
@@ -13,7 +13,7 @@ from inferelator_ng.distributed.inferelator_mp import MPControl
 my_dir = os.path.dirname(__file__)
 
 
-class TestWorkflowStartup(unittest.TestCase):
+class TestWorkflow(unittest.TestCase):
 
     def setUp(self):
         self.workflow = workflow.WorkflowBase()
@@ -32,6 +32,13 @@ class TestWorkflowStartup(unittest.TestCase):
         self.assertEqual(len(self.workflow.tf_names), 100)
         tf_names = list(map(lambda x: "G" + str(x), list(range(1, 101))))
         self.assertListEqual(self.workflow.tf_names, tf_names)
+
+    def test_load_priors_gs(self):
+        self.workflow.set_gold_standard_and_priors()
+        self.assertEqual(self.workflow.priors_data.shape, (100, 100))
+        self.assertEqual(self.workflow.gold_standard.shape, (100, 100))
+        self.assertTrue(all(self.workflow.priors_data.index == self.workflow.priors_data.columns))
+        self.assertTrue(all(self.workflow.gold_standard.index == self.workflow.gold_standard.columns))
 
     def test_load_metadata(self):
         self.workflow.read_metadata()
@@ -52,88 +59,88 @@ class TestWorkflowStartup(unittest.TestCase):
             self.workflow.startup_finish()
         with self.assertRaises(NotImplementedError):
             self.workflow.run()
+        with self.assertRaises(NotImplementedError):
+            self.workflow.emit_results()
 
+    def test_append_path(self):
+        self.workflow.append_to_path('input_dir', 'test')
+        self.assertEqual(os.path.join(my_dir, "../../data/dream4", 'test'), self.workflow.input_dir)
+        self.workflow.input_dir = None
+        with self.assertRaises(ValueError):
+            self.workflow.append_to_path('input_dir', 'test')
 
-class StubWorkflow(workflow.WorkflowBase):
-    """
-    Artificial work flow for logic testing.
-    """
+    def test_make_fake_metadata(self):
+        self.workflow.read_expression()
+        self.workflow.meta_data = self.workflow.create_default_meta_data(self.workflow.expression_matrix)
+        self.assertEqual(self.workflow.meta_data.shape, (421, 5))
 
-    cd_called = False
-    test_case = None
+    def test_prior_split(self):
+        self.workflow.split_priors_for_gold_standard = True
+        self.workflow.cv_split_ratio = 0.5
+        self.workflow.set_gold_standard_and_priors()
+        self.assertEqual(self.workflow.priors_data.shape, (50, 100))
+        self.assertEqual(self.workflow.gold_standard.shape, (50, 100))
+        self.assertTrue(all(self.workflow.priors_data.columns == self.workflow.gold_standard.columns))
 
-    def __init__(self):
-        pass
+    def test_cv_split(self):
+        self.workflow.split_gold_standard_for_crossvalidation = True
+        self.workflow.cv_split_ratio = 0.5
+        self.workflow.set_gold_standard_and_priors()
+        self.assertEqual(self.workflow.priors_data.shape, (50, 100))
+        self.assertEqual(self.workflow.gold_standard.shape, (50, 100))
+        self.assertTrue(all(self.workflow.priors_data.columns == self.workflow.gold_standard.columns))
 
-    def run(self):
-        self.get_data()
+    def test_filter_priors(self):
+        self.workflow.read_expression()
+        self.workflow.read_tfs()
+        self.workflow.split_priors_for_gold_standard = True
+        self.workflow.cv_split_ratio = 0.5
+        self.workflow.cv_split_axis = 1
+        self.workflow.set_gold_standard_and_priors()
+        self.assertEqual(self.workflow.priors_data.shape, (100, 50))
+        self.workflow.filter_expression_and_priors()
+        self.assertEqual(self.workflow.priors_data.shape, (100, 100))
 
-    def compute_common_data(self):
-        cd_called = True
+    def test_get_bootstraps(self):
+        bootstrap_0 = [37, 235, 396, 72, 255, 393, 203, 133, 335, 144, 129, 71, 237, 390, 281, 178, 276, 254, 357, 402,
+                       395, 252, 156, 413, 398, 50, 68, 215, 241, 352, 86, 141, 393, 7, 319, 317, 22, 313, 1, 384, 316,
+                       209, 264, 216, 141, 115, 121, 30, 71, 387, 405, 49, 313, 3, 280, 43, 76, 26, 308, 336, 109, 371,
+                       297, 15, 64, 196, 25, 367, 226, 391, 282, 153, 104, 22, 265, 195, 126, 279, 381, 356, 155, 313,
+                       83, 166, 136, 288, 418, 266, 279, 143, 239, 87, 281, 243, 348, 74, 190, 302, 416, 216, 151, 183,
+                       321, 369, 333, 259, 384, 253, 262, 52, 2, 76, 149, 203, 263, 77, 200, 75, 332, 43, 20, 30, 36,
+                       359, 263, 301, 57, 240, 210, 96, 269, 10, 279, 380, 337, 391, 377, 152, 202, 148, 416, 140, 193,
+                       94, 60, 152, 338, 371, 353, 130, 220, 103, 354, 266, 182, 352, 338, 198, 194, 327, 176, 54, 15,
+                       389, 401, 170, 20, 118, 278, 397, 114, 97, 181, 340, 10, 96, 183, 317, 56, 217, 405, 231, 96, 25,
+                       398, 141, 212, 116, 299, 134, 205, 184, 399, 24, 137, 199, 309, 325, 420, 357, 248, 21, 296, 77,
+                       219, 177, 369, 303, 45, 343, 144, 412, 234, 45, 372, 322, 302, 384, 413, 63, 331, 35, 33, 130,
+                       83, 48, 310, 288, 253, 156, 55, 210, 287, 28, 222, 330, 136, 109, 99, 32, 8, 84, 50, 79, 169,
+                       320, 108, 211, 24, 113, 276, 44, 271, 158, 398, 275, 251, 154, 235, 86, 391, 227, 53, 366, 243,
+                       290, 100, 228, 288, 403, 280, 211, 229, 94, 166, 175, 231, 389, 79, 63, 369, 87, 416, 298, 202,
+                       194, 216, 226, 158, 145, 324, 320, 188, 206, 145, 167, 163, 156, 150, 294, 169, 205, 326, 153,
+                       230, 240, 48, 178, 300, 105, 182, 256, 342, 272, 275, 265, 112, 220, 51, 367, 138, 123, 324, 407,
+                       270, 191, 21, 174, 380, 131, 344, 208, 54, 71, 14, 205, 143, 409, 309, 212, 114, 238, 413, 44,
+                       37, 150, 332, 12, 376, 315, 410, 215, 125, 43, 370, 294, 91, 112, 253, 325, 226, 417, 171, 410,
+                       184, 73, 308, 89, 27, 43, 1, 338, 127, 94, 410, 340, 30, 359, 64, 150, 98, 308, 131, 70, 140,
+                       295, 230, 83, 239, 176, 317, 269, 164, 279, 406, 122, 249, 351, 53, 393, 169, 344, 365, 246, 221,
+                       244, 204, 338, 362, 395, 105, 36, 112, 144, 158, 115, 106, 212, 291, 337, 258]
 
-    def get_priors(self):
-        return [StubPrior(1), StubPrior(2)]
+        self.workflow.read_expression()
+        self.workflow.response = self.workflow.expression_matrix
+        self.workflow.random_seed = 1
+        self.workflow.num_bootstraps = 5
+        bootstraps = self.workflow.get_bootstraps()
+        self.assertEqual(len(bootstraps), 5)
+        self.assertListEqual(bootstraps[0], bootstrap_0)
 
-    def get_bootstraps(self):
-        return [StubBootstrap("A"), StubBootstrap("B")]
+    def test_is_master(self):
+        self.assertTrue(self.workflow.is_master())
 
-    def emit_results(self, priors):
-        # check that everything got initialized properly
-        assert self.exp_mat is not None
-        assert self.meta_data is not None
-        assert self.tf_names is not None
-        assert self.priors_data is not None
-        assert self.gold_standard is not None
-        test = self.test_case
-        idents = [1, 2]
-        names = ["A", "B"]
-        test.assertEqual([p.ident for p in priors], idents)
-        for p in priors:
-            test.assertEqual(p.names, names)
+    def test_make_output_dir(self):
+        temp_dir = tempfile.mkdtemp()
+        self.workflow.input_dir = temp_dir
+        self.workflow.create_output_dir()
 
+        self.assertTrue(os.path.exists(self.workflow.output_dir))
+        os.rmdir(self.workflow.output_dir)
+        os.rmdir(temp_dir)
 
-class StubBootstrap(object):
-
-    def __init__(self, name):
-        self.name = name
-
-
-class StubPrior(object):
-    """
-    Completely artificial prior object shell implementation.
-    """
-
-    def __init__(self, ident):
-        self.ident = ident
-
-    names = []
-
-    def run_bootstrap(self, workflow, bootstrap_parameters):
-        self.names = self.names + [bootstrap_parameters.name]
-
-    bootstrap_results = None
-
-    def combine_bootstrap_results(self, workflow, results):
-        self.bootstrap_results = results
-
-
-class TestWorkflowStub(unittest.TestCase):
-
-    def test_stub(self):
-        # create and configure the work flow
-        work = StubWorkflow()
-        work.input_dir = os.path.join(my_dir, "../../data/dream4")
-        work.test_case = self
-        # run the workflow (validation tests in emit_results)
-        work.run()
-
-    def test_stub_without_metadata(self):
-        # create and configure the work flow
-        work = StubWorkflow()
-        work.input_dir = os.path.join(my_dir, "../../data/dream4_no_metadata_for_test_purposes")
-        work.test_case = self
-        # run the workflow (validation tests in emit_results)
-        work.run()
-        self.assertEqual(work.meta_data.shape, (421, 5))
-        self.assertEqual(set(work.meta_data.columns.tolist()),
-                         set(['condName', 'del.t', 'is1stLast', 'isTs', 'prevCol']))
