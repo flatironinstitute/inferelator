c6a47dd6374900bff81e0e97053b5937e688736d
CJackson <christopher@cajackson.org>
2019-03-28 15:39:06 -0400

    Remove obsolete test

--- inferelator_ng/tests/test_design_response.py
+++ inferelator_ng/tests/test_design_response.py
@@ -7,12 +7,14 @@ from inferelator_ng import utils
 
 my_dir = os.path.dirname(__file__)
 
+
 class TestDR(unittest.TestCase):
     """
     Superclass for common methods
     """
+
     def calculate_design_and_response(self):
-        #drd = design_response_R.DRDriver()
+        # drd = design_response_R.DRDriver()
         drd = design_response_translation.PythonDRDriver()
         target = drd.target_directory = os.path.join(my_dir, "artifacts")
         if not os.path.exists(target):
@@ -22,35 +24,6 @@ class TestDR(unittest.TestCase):
         drd.tau = self.tau
         (self.design, self.response) = drd.run(self.exp, self.meta)
 
-@unittest.skip("I don't know why the special character thing is necessary so I didn't reimplement it")
-class TestSpecialCharacter(TestDR):
-
-    def setUp(self):
-        spchrs='~!@#$%^&*()_-+=|\}]{[;:/?.><\\'
-        self.meta = pd.DataFrame()
-        self.meta['isTs']=[True, True, True, True, False]
-        self.meta['is1stLast'] = ['f','m','m','l','e']
-        self.meta['prevCol'] = ['NA','ts1'+spchrs,'ts2'+spchrs,'ts3'+spchrs, 'NA']
-        self.meta['del.t'] = ['NA', 3, 2, 5, 'NA']
-        self.meta['condName'] = ['ts1'+spchrs,'ts2'+spchrs,'ts3'+spchrs,'ts4'+spchrs,'ss']
-        self.exp = pd.DataFrame(np.reshape(range(10), (2,5)) + 1,
-            index = ['gene' + str(i + 1) + spchrs for i in range(2)],
-            columns = ['ts' + str(i + 1) + spchrs for i in range(4)] + ['ss'])
-        self.delT_min = 2
-        self.delT_max = 4
-        self.tau = 2
-        self.calculate_design_and_response()
-
-    def testspecialcharacter(self):
-        spchrs='~!@#$%^&*()_-+=|\}]{[;:/?.><\\'
-        ds, resp = (self.design, self.response)
-        expression_1 = np.array(list(self.exp['ts1' + spchrs]))
-        expression_2 = np.array(list(self.exp['ts2' + spchrs]))
-        expected_response_1 = (expression_1 + self.tau * (expression_2 - expression_1) / (float(self.meta['del.t'][1])))
-        expression_3 = np.array(list(self.exp['ts3' + spchrs]))
-        expected_response_2 = expression_2 + self.tau * (expression_3 - expression_2) /  (float(self.meta['del.t'][2]))
-        np.testing.assert_almost_equal(np.array(resp['ts1' + spchrs]), expected_response_1)
-        np.testing.assert_almost_equal(np.array(resp['ts2' + spchrs]), expected_response_2)
 
 @unittest.skip("Need to redo this to match the current drd")
 class TestDRModelOrganisms(TestDR):
@@ -70,18 +43,19 @@ class TestDRModelOrganisms(TestDR):
         self.assertEqual(expected_response.index.tolist(), self.response.index.tolist())
         self.assertTrue(pd.DataFrame.equals(expected_design, self.design))
 
+
 class TestDRAboveDeltMax(TestDR):
 
     def setUp(self):
         self.meta = pd.DataFrame()
-        self.meta['isTs']=[True, True, True, True, False]
-        self.meta['is1stLast'] = ['f','m','m','l','e']
-        self.meta['prevCol'] = ['NA','ts1','ts2','ts3', 'NA']
+        self.meta['isTs'] = [True, True, True, True, False]
+        self.meta['is1stLast'] = ['f', 'm', 'm', 'l', 'e']
+        self.meta['prevCol'] = ['NA', 'ts1', 'ts2', 'ts3', 'NA']
         self.meta['del.t'] = ['NA', 3, 2, 5, 'NA']
-        self.meta['condName'] = ['ts1','ts2','ts3','ts4','ss']
-        self.exp = pd.DataFrame(np.reshape(range(10), (2,5)) + 1,
-            index = ['gene' + str(i + 1) for i in range(2)],
-            columns = ['ts' + str(i + 1) for i in range(4)] + ['ss'])
+        self.meta['condName'] = ['ts1', 'ts2', 'ts3', 'ts4', 'ss']
+        self.exp = pd.DataFrame(np.reshape(range(10), (2, 5)) + 1,
+                                index=['gene' + str(i + 1) for i in range(2)],
+                                columns=['ts' + str(i + 1) for i in range(4)] + ['ss'])
         self.delT_min = 2
         self.delT_max = 4
         self.tau = 2
@@ -92,11 +66,11 @@ class TestDRAboveDeltMax(TestDR):
         ds, resp = (self.design, self.response)
         self.assertEqual(ds.shape, (2, 4))
         self.assertEqual(list(ds.columns), ['ts1-ts2', 'ts2-ts3', 'ss', 'ts4'],
-            msg = "Guarantee that the ts3-ts4 condition is dropped, "
-                  "since its delT of 5 is greater than delt_max of 4")
+                         msg="Guarantee that the ts3-ts4 condition is dropped, "
+                             "since its delT of 5 is greater than delt_max of 4")
         self.assertEqual(list(ds['ss']), [5, 10])
         self.assertEqual(list(ds['ss']), list(resp['ss']),
-            msg = 'Steady State design and response should be equal')
+                         msg='Steady State design and response should be equal')
         self.assertTrue((resp['ts2-ts3'].values == [3, 8]).all())
 
     def test_response_matrix_steady_state_above_delt_max(self):
@@ -112,18 +86,19 @@ class TestDRAboveDeltMax(TestDR):
         expected_response_1 = (expression_1 + self.tau * (expression_2 - expression_1) / (
             float(self.meta['del.t'][1])))
         expression_3 = np.array(list(self.exp['ts3']))
-        expected_response_2 = expression_2 + self.tau * (expression_3 - expression_2) /  (
+        expected_response_2 = expression_2 + self.tau * (expression_3 - expression_2) / (
             float(self.meta['del.t'][2]))
         np.testing.assert_almost_equal(np.array(resp['ts1-ts2']), expected_response_1)
         np.testing.assert_almost_equal(np.array(resp['ts2-ts3']), expected_response_2)
+
 
 class TestDRMicro(TestDR):
 
     def setUp(self):
         self.meta = pd.DataFrame()
-        self.meta['isTs']=[False, False]
-        self.meta['is1stLast'] = ['e','e']
-        self.meta['prevCol'] = ['NA','NA']
+        self.meta['isTs'] = [False, False]
+        self.meta['is1stLast'] = ['e', 'e']
+        self.meta['prevCol'] = ['NA', 'NA']
         self.meta['del.t'] = ['NA', 'NA']
         self.meta['condName'] = ['ss1', 'ss2']
         self.exp = pd.DataFrame(
@@ -143,13 +118,14 @@ class TestDRMicro(TestDR):
         # In steady state, expect design and response to be identical
         self.assertTrue(ds.equals(resp))
 
+
 class TestDRBelowDeltMin(TestDR):
 
     def setUp(self):
         self.meta = pd.DataFrame()
-        self.meta['isTs']=[True, True, True, True, False]
-        self.meta['is1stLast'] = ['f','m','m','l','e']
-        self.meta['prevCol'] = ['NA','ts1','ts2','ts3', 'NA']
+        self.meta['isTs'] = [True, True, True, True, False]
+        self.meta['is1stLast'] = ['f', 'm', 'm', 'l', 'e']
+        self.meta['prevCol'] = ['NA', 'ts1', 'ts2', 'ts3', 'NA']
         self.meta['del.t'] = ['NA', 1, 2, 3, 'NA']
         self.meta['condName'] = ['ts1', 'ts2', 'ts3', 'ts4', 'ss']
         self.exp = pd.DataFrame(
@@ -166,29 +142,31 @@ class TestDRBelowDeltMin(TestDR):
         ds, resp = (self.design, self.response)
         expression_1 = np.array(list(self.exp['ts1']))
         expression_3 = np.array(list(self.exp['ts3']))
-        expected_response_1 = expression_1 + self.tau * (expression_3 - expression_1) /  (float(self.meta['del.t'][1]) + float(self.meta['del.t'][2]))
+        expected_response_1 = expression_1 + self.tau * (expression_3 - expression_1) / (
+                    float(self.meta['del.t'][1]) + float(self.meta['del.t'][2]))
         np.testing.assert_almost_equal(np.array(resp['ts1-ts3']), expected_response_1)
-        #pdb.set_trace()
+        # pdb.set_trace()
 
     @unittest.skip("skipping until we've determined if we want to modify the legacy R code")
     def test_design_matrix_headers_below_delt_min(self):
         ds, resp = (self.design, self.response)
         print(ds.columns)
         self.assertEqual(list(ds.columns), ['ss', 'ts1', 'ts2', 'ts3'],
-            msg = "Guarantee that the ts4 condition is dropped, since its the last in the time series")
+                         msg="Guarantee that the ts4 condition is dropped, since its the last in the time series")
+
 
 class TestBranchingTimeSeries(TestDR):
 
     def setUp(self):
         self.meta = pd.DataFrame()
-        self.meta['isTs']=[True, True, True]
-        self.meta['is1stLast'] = ['f','l','l']
-        self.meta['prevCol'] = ['NA','ts1','ts1']
+        self.meta['isTs'] = [True, True, True]
+        self.meta['is1stLast'] = ['f', 'l', 'l']
+        self.meta['prevCol'] = ['NA', 'ts1', 'ts1']
         self.meta['del.t'] = ['NA', 2, 2]
-        self.meta['condName'] = ['ts1','ts2','ts3']
-        self.exp = pd.DataFrame(np.reshape(range(9), (3,3)) + 1,
-            index = ['gene' + str(i + 1) for i in range(3)],
-            columns = ['ts' + str(i + 1) for i in range(3)])
+        self.meta['condName'] = ['ts1', 'ts2', 'ts3']
+        self.exp = pd.DataFrame(np.reshape(range(9), (3, 3)) + 1,
+                                index=['gene' + str(i + 1) for i in range(3)],
+                                columns=['ts' + str(i + 1) for i in range(3)])
         self.delT_min = 1
         self.delT_max = 4
         self.tau = 1
@@ -198,10 +176,11 @@ class TestBranchingTimeSeries(TestDR):
         ds, resp = (self.design, self.response)
         self.assertEqual(ds.shape, (3, 2))
         self.assertEqual(list(ds.columns), ['ts1-ts2', 'ts1-ts3'],
-             msg = 'This is how the R code happens to name branching time series')
+                         msg='This is how the R code happens to name branching time series')
         for col in ds:
             self.assertEqual(list(ds[col]), list(self.exp['ts1']),
-                msg = '{} column in the design matrix should be equal to the branching source, ts1, in the exp matrix'.format(col))
+                             msg='{} column in the design matrix should be equal to the branching source, ts1, in the exp matrix'.format(
+                                 col))
 
     def test_response_matrix_branching_time_series(self):
         ds, resp = (self.design, self.response)
@@ -209,11 +188,11 @@ class TestBranchingTimeSeries(TestDR):
         expression_1 = np.array(list(self.exp['ts1']))
         expression_2 = np.array(list(self.exp['ts2']))
         expected_response_1 = (expression_1 + self.tau * (expression_2 - expression_1) /
-            float(self.meta['del.t'][1]))
+                               float(self.meta['del.t'][1]))
 
         expression_3 = np.array(list(self.exp['ts3']))
         expected_response_2 = (expression_1 + self.tau * (expression_3 - expression_1) /
-            float(self.meta['del.t'][2]))
+                               float(self.meta['del.t'][2]))
 
         np.testing.assert_almost_equal(np.array(resp['ts1-ts2']), expected_response_1)
         np.testing.assert_almost_equal(np.array(resp['ts1-ts3']), expected_response_2)
