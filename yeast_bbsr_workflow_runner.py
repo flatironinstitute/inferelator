1a8353012ee08032ed9030388a22a71f1bc2ce1d
CJackson <christopher@cajackson.org>
2019-03-29 13:25:35 -0400

    Yeast example workflow

--- yeast_bbsr_workflow_runner.py
+++ yeast_bbsr_workflow_runner.py
@@ -1,22 +1,15 @@
 from inferelator_ng import workflow
-from inferelator_ng.distributed.inferelator_mp import MPControl
-from inferelator_ng import utils
-
-utils.Debug.set_verbose_level(1)
-MPControl.set_multiprocess_engine("dask-local")
-MPControl.client.processes = 1
-MPControl.client.local_dir = '/tmp'
-MPControl.connect()
 
 wflow = workflow.inferelator_workflow(regression="bbsr", workflow="tfa")
 # Common configuration parameters
-wflow.input_dir = '~/PycharmProjects/inferelator_sc/data'
-wflow.append_to_path('input_dir', 'bsubtilis')
+wflow.input_dir = 'data/yeast'
+wflow.priors_file = 'yeast-motif-prior.tsv'
+wflow.multiprocessing_controller = "local"
 wflow.num_bootstraps = 2
 wflow.delTmax = 110
 wflow.delTmin = 0
 wflow.tau = 45
-wflow.gold_standard_filter_method = 'keep_all_gold_standard'
 
 if __name__ == "__main__":
     wflow.run()
+
