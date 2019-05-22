65a4a32aee47ba4a466260935e1aa9203789b493
Claudia Skok Gibbs <cskokgibbs@ccbmac072.scdanet.org>
2019-05-22 11:31:30 -0400

    fixing elastic net test

--- inferelator/tests/test_regression.py
+++ inferelator/tests/test_regression.py
@@ -13,9 +13,9 @@ import pandas as pd
 class TestRegressionFactory(unittest.TestCase):
 
     def setUp(self):
-        self.expr = pd.DataFrame([[2, 28, 0, 16, 1, 3], [6, 21, 0, 3, 1, 3], [4, 39, 0, 17, 1, 3],
-                                  [8, 34, 0, 7, 1, 3], [6, 26, 0, 3, 1, 3], [1, 31, 0, 1, 1, 4],
-                                  [3, 27, 0, 5, 1, 4], [8, 34, 0, 9, 1, 3], [1, 22, 0, 3, 1, 4],
+        self.expr = pd.DataFrame([[2, 28, 1, 16, 2, 3], [6, 21, 1, 3, 0, 3], [4, 39, 0, 17, 1, 3],
+                                  [8, 34, 0, 7, 0, 3], [6, 26, 0, 3, 1, 3], [1, 31, 0, 1, 1, 4],
+                                  [3, 27, 1, 5, 2, 4], [8, 34, 2, 9, 2, 3], [1, 22, 2, 3, 3, 4],
                                   [9, 33, 0, 17, 1, 2]],
                                  columns=["gene1", "gene2", "gene3", "gene4", "gene5", "gene6"])
         self.meta = pd.DataFrame({"Condition": ["A", "B", "C", "C", "B", "B", "A", "C", "B", "C"],
@@ -50,3 +50,14 @@ class TestRegressionFactory(unittest.TestCase):
         self.workflow.run()
         self.assertEqual(self.workflow.aupr, 1)
 
+    def test_elasticnet(self):
+
+        self.workflow = create_puppet_workflow(base_class=tfa_workflow.TFAWorkFlow,
+                                               regression_class=elasticnet_python.ElasticNetWorkflow)
+        self.workflow = self.workflow(self.expr.transpose(), self.meta, self.prior, self.gold_standard)
+        self.workflow.gene_list = self.gene_list
+        self.workflow.tf_names = self.tf_names
+        self.workflow.meta_data_file = None
+        self.workflow.read_metadata()
+        self.workflow.run()
+        self.assertEqual(self.workflow.aupr, 1)
