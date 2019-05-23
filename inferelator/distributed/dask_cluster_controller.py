07eec55d71a6aedd2d6bd2a6361f368e38ad2fad
CJackson <christopher@cajackson.org>
2019-05-22 20:33:45 -0400

    Change to PYTHONOPTIMIZE env flag

--- inferelator/distributed/dask_cluster_controller.py
+++ inferelator/distributed/dask_cluster_controller.py
@@ -22,7 +22,8 @@ DEFAULT_WALLTIME = '1:00:00'
 ENV_EXTRA = ['module purge',
              'export MKL_NUM_THREADS=1',
              'export OPENBLAS_NUM_THREADS=1',
-             'export NUMEXPR_NUM_THREADS=1']
+             'export NUMEXPR_NUM_THREADS=1',
+             'export PYTHONOPTIMIZE=1']
 
 CONTROLLER_EXTRA = []
 
@@ -111,7 +112,6 @@ class DaskHPCClusterController(AbstractController):
     cluster_controller_options = CONTROLLER_EXTRA
     interface = DEFAULT_INTERFACE
     local_directory = DEFAULT_LOCAL_DIR
-    python = sys.executable + " -O"
 
     @classmethod
     def connect(cls, *args, **kwargs):
@@ -124,8 +124,7 @@ class DaskHPCClusterController(AbstractController):
                                                          job_cpu=cls.job_cpu, cores=cls.cores, processes=cls.processes,
                                                          job_mem=cls.job_mem, env_extra=cls.env_extra,
                                                          interface=cls.interface, local_directory=cls.local_directory,
-                                                         memory=cls.memory, job_extra=cls.cluster_controller_options,
-                                                         python=cls.python)
+                                                         memory=cls.memory, job_extra=cls.cluster_controller_options)
 
         # Deactivate the worker memory nanny
         if cls.worker_memory_limit == 0:
