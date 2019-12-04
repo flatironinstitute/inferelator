6e0928f55801f3a1b8228e2ebd5715320f0d4012
asistradition <asistradition@users.noreply.github.com>
2019-12-04 10:35:13 -0500

    Changed test so that it's clearer what the goal is

--- inferelator/tests/test_elasticnet_python.py
+++ inferelator/tests/test_elasticnet_python.py
@@ -4,34 +4,59 @@ import pandas as pd
 import numpy as np
 import os
 
+TEST_SEED = 50
+
+PREDICT_ARRAY = np.random.RandomState(TEST_SEED).randn(100, 5)
+RESPONSE_ARRAY = np.random.RandomState(TEST_SEED).randn(100, 1)
+RESPONSE_ARRAY[:, 0] = np.sort(RESPONSE_ARRAY[:, 0])
+
+PARAMS = {"l1_ratio": [0.5, 0.7, 0.9],
+          'eps': 0.001,
+          'n_alphas': 50,
+          'alphas': None,
+          'fit_intercept': True,
+          'normalize': False,
+          'precompute': 'auto',
+          'max_iter': 1000,
+          'tol': 0.001,
+          'cv': 3,
+          'copy_X': True,
+          'verbose': 0,
+          'n_jobs': 1,
+          'positive': False,
+          'random_state': 99,
+          'selection': 'random'}
+
+
 class TestElasticNet(unittest.TestCase):
 
-    def test_elastic_net(self):
-        X = np.array([[0, 1, 0, 1, 0, 1], [2, 3, 2, 3, 2, 3], [4, 5, 4, 5, 4, 5], [6, 7, 6, 7, 6, 7],
-                      [8, 9, 8, 9, 8, 9]])
-        Y = np.array([[1, 2, 3, 4, 5, 6]])
-        params = {"l1_ratio":[0.5, 0.7, 0.9],
-                             'eps': 0.001,
-                             'n_alphas': 50,
-                             'alphas': None,
-                             'fit_intercept': True,
-                             'normalize': False,
-                             'precompute': 'auto',
-                             'max_iter': 1000,
-                             'tol': 0.001,
-                             'cv': 3,
-                             'copy_X': True,
-                             'verbose': 0,
-                             'n_jobs': 1,
-                             'positive': False,
-                             'random_state': 99,
-                             'selection': 'random'}
+    def test_elastic_net_sklearn_zeros(self):
+        x = PREDICT_ARRAY.copy()
+        y = RESPONSE_ARRAY.copy()
 
-        raw_result = elasticnet_python.ElasticNetCV(**params).fit(X.T, Y.flatten())
+        raw_result = elasticnet_python.ElasticNetCV(**PARAMS).fit(x, y.flatten())
         np.testing.assert_array_equal([0.0, 0.0, 0.0, 0.0, 0.0], raw_result.coef_)
 
-        result = elasticnet_python.elastic_net(X, Y, params)
-        print(result)
+    def test_elastic_net_sklearn_nz(self):
+        x = PREDICT_ARRAY.copy()
+        y = RESPONSE_ARRAY.copy()
+
+        x[:, 2] = np.sort(x[:, 2])
+
+        raw_result = elasticnet_python.ElasticNetCV(**PARAMS).fit(x, y.flatten())
+        print(raw_result.coef_)
+        np.testing.assert_array_equal([False, False, True, False, False], abs(raw_result.coef_) > 0.1)
+
+    def test_elastic_net_zeros(self):
+        x = PREDICT_ARRAY.copy()
+        y = RESPONSE_ARRAY.copy()
+
+        result = elasticnet_python.elastic_net(x.T, y, PARAMS)
+
+        self.assertEqual(len(result["pp"]), 5)
+        self.assertEqual(len(result["betas"]), 5)
+        self.assertEqual(len(result["betas_resc"]), 5)
+
         pp = np.array([True, True, True, True, True])
         betas = ([0.0, 0.0, 0.0, 0.0, 0.0])
         betas_resc = ([0.0, 0.0, 0.0, 0.0, 0.0])
@@ -40,35 +65,21 @@ class TestElasticNet(unittest.TestCase):
             for idx in range(0, len(check[component])):
                 np.testing.assert_array_almost_equal(result[component][idx], check[component][idx], 2)
 
-    def test_elastic_net_result(self):
-        X = np.array([[10, 1, 5, 2, 7, 1], [15, 3, 12, 3, 11, 3], [4, 5, 0, 5, 4, 10], [6, 0, 3, 7, 6, 11],
-                      [8, 15, 8, 10, 0, 9]])
-        Y = np.array([[10, 2, 12, 4, 5, 6]])
-        params = {"l1_ratio":[0.5, 0.7, 0.9],
-                             'eps': 0.001,
-                             'n_alphas': 50,
-                             'alphas': None,
-                             'fit_intercept': True,
-                             'normalize': False,
-                             'precompute': 'auto',
-                             'max_iter': 1000,
-                             'tol': 0.001,
-                             'cv': 3,
-                             'copy_X': True,
-                             'verbose': 0,
-                             'n_jobs': 1,
-                             'positive': False,
-                             'random_state': 99,
-                             'selection': 'random'}
+    def test_elastic_net_nz(self):
+        x = PREDICT_ARRAY.copy()
+        y = RESPONSE_ARRAY.copy()
 
-        raw_result = elasticnet_python.ElasticNetCV(**params).fit(X.T, Y.flatten())
-        np.testing.assert_almost_equal([0.0, 0.354414, 0.0, 0.0, 0.0], raw_result.coef_, 3)
+        x[:, 2] = np.sort(x[:, 2])
 
-        result = elasticnet_python.elastic_net(X, Y, params)
-        print(result)
-        pp = np.array([False, True, False, False, False])
-        betas = ([0.74468085])
-        betas_resc = ([0.50166146])
+        result = elasticnet_python.elastic_net(x.T, y, PARAMS)
+
+        self.assertEqual(len(result["pp"]), 5)
+        self.assertEqual(len(result["betas"]), 1)
+        self.assertEqual(len(result["betas_resc"]), 1)
+
+        pp = np.array([False, False, True, False, False])
+        betas = ([1.05])
+        betas_resc = ([0.97])
         check = {'pp': pp, 'betas': betas, 'betas_resc': betas_resc}
         for component in check.keys():
             for idx in range(0, len(check[component])):
