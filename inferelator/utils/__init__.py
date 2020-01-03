a66560021ad7938018e0626128fcc474c873ce9e
asistradition <asistradition@users.noreply.github.com>
2020-01-02 20:33:26 -0500

    Replace lost function

--- inferelator/utils/__init__.py
+++ inferelator/utils/__init__.py
@@ -1,4 +1,5 @@
 from inferelator.utils.validator import Validator, is_string
 from inferelator.utils.debug import Debug, slurm_envs
-from inferelator.utils.data import InferelatorData, df_from_tsv, df_set_diag, melt_and_reindex_dataframe, make_array_2d
+from inferelator.utils.data import (InferelatorData, df_from_tsv, df_set_diag, melt_and_reindex_dataframe,
+                                    make_array_2d, transpose_dataframe)
 
