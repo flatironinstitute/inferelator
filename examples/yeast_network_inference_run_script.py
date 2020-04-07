ee3cc37038aeaddecc190bdf0c7ea5b919dde9c2
asistradition <asistradition@users.noreply.github.com>
2020-04-07 16:14:50 -0400

    Updated examples

--- examples/yeast_network_inference_run_script.py
+++ examples/yeast_network_inference_run_script.py
@@ -1,3 +1,9 @@
+import os
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
 # Load modules
 from inferelator import utils
 from inferelator.distributed.inferelator_mp import MPControl
@@ -38,37 +44,38 @@ if __name__ == '__main__' and local_engine:
 
 
 # Define the general run parameters
+# This function will take a workflow and set the file paths
+# As well as a 5-fold cross validation
+
 def set_up_workflow(wkf):
     wkf.set_file_paths(input_dir=DATA_DIR,
                        output_dir=OUTPUT_DIR,
-                       expression_matrix_file=EXPRESSION_FILE_NAME,
                        tf_names_file=TF_LIST_FILE_NAME,
                        meta_data_file=META_DATA_FILE_NAME,
                        priors_file=PRIORS_FILE_NAME,
                        gold_standard_file=GOLD_STANDARD_FILE_NAME)
+    wkf.set_expression_file(tsv=EXPRESSION_FILE_NAME)
     wkf.set_file_properties(expression_matrix_columns_are_genes=False)
     wkf.set_run_parameters(num_bootstraps=5)
     wkf.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True, cv_split_ratio=0.2)
     return wkf
+
 
 # Inference with BBSR (crossvalidation)
 # Using the crossvalidation wrapper
 # Run the regression 10 times and hold 20% of the gold standard out of the priors for testing each time
 # Each run is seeded differently (and therefore has different holdouts)
 
-# Create a crossvalidation wrapper
-cv_wrap = CrossValidationManager()
-
-# Assign variables for grid search
-cv_wrap.add_gridsearch_parameter('random_seed', CV_SEEDS)
-
 # Create a worker
 worker = workflow.inferelator_workflow(regression="bbsr", workflow="tfa")
 worker = set_up_workflow(worker)
 worker.append_to_path("output_dir", "bbsr")
 
-# Assign the worker to the crossvalidation wrapper
-cv_wrap.workflow = worker
+# Create a crossvalidation wrapper
+cv_wrap = CrossValidationManager(worker)
+
+# Assign variables for grid search
+cv_wrap.add_gridsearch_parameter('random_seed', CV_SEEDS)
 
 # Run
 cv_wrap.run()
@@ -87,7 +94,7 @@ worker.append_to_path("output_dir", "elastic_net")
 # Parameters set with this function are passed to sklearn.linear_model.ElasticNetCV
 worker.set_regression_parameters(l1_ratio=1, max_iter=2000)
 
-# Create a crossvalidation wrapper and pass it the worker during __init__
+# Create a crossvalidation wrapper
 cv_wrap = CrossValidationManager(worker)
 
 # Assign variables for grid search
