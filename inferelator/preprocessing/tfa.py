049ea5304394b2f132dd59400d8cf371ea6a0b84
Claudia Skok Gibbs <cskokgibbs@flatironinstitute.org>
2020-06-08 15:36:48 -0400

    fixing truncated svd

--- inferelator/preprocessing/tfa.py
+++ inferelator/preprocessing/tfa.py
@@ -87,10 +87,10 @@ class truncated_svd_TFA(TFA):
     @staticmethod
     def _calculate_activity(prior, expression_data):
         tf_length = prior.shape[1]
-        svd = TruncatedSVD(n_components=tf_length, n_iter=7, random_state=42)
-        svd.fit(expression_data.values)
-        transformed = TruncatedSVD.transform(svd, expression_data.values)
-        activity = np.linalg.pinv(prior) @ transformed
+        svd = TruncatedSVD(n_components=tf_length)
+        c = svd.fit_transform(expression_data.values)
+        d = svd.inverse_transform(c)
+        activity = np.linalg.pinv(prior) @ d
         activity = activity.T
         return activity
 
