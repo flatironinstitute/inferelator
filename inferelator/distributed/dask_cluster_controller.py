b6c3508e02eec03acdf32eb1a2d6230cafe9fe30
asistradition <asistradition@users.noreply.github.com>
2020-07-29 15:57:13 -0400

    Print dask status on connect

--- inferelator/distributed/dask_cluster_controller.py
+++ inferelator/distributed/dask_cluster_controller.py
@@ -144,6 +144,9 @@ class DaskHPCClusterController(AbstractController):
         cls._add_local_node_workers(cls._num_local_workers)
         cls._tracker = WorkerTracker()
 
+        utils.Debug.vprint("Dask scheduler & client active", level=0)
+        utils.Debug.vprint(cls.client.scheduler_info()['services'], level=0)
+
         return True
 
     @classmethod
