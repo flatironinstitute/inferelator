0030b06f5fb1d66cfa50dd1bab7ab9af39674048
asistradition <asistradition@users.noreply.github.com>
2020-11-05 13:23:52 -0500

    Export combinedmetric

--- inferelator/postprocessing/__init__.py
+++ inferelator/postprocessing/__init__.py
@@ -1,4 +1,4 @@
 from inferelator.postprocessing.column_names import *
-from inferelator.postprocessing.model_metrics import MetricHandler, RankSummingMetric
+from inferelator.postprocessing.model_metrics import MetricHandler, RankSummingMetric, CombinedMetric
 from inferelator.postprocessing.results_processor import ResultsProcessor
 from inferelator.postprocessing.results_processor_mtl import ResultsProcessorMultiTask
