8b6b489aa2ea14ecc7d9023abb3d9027aae5df0b
asistradition <asistradition@users.noreply.github.com>
2020-11-23 15:59:51 -0500

    Increment version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Flatiron Institute'
 author = 'Chris Jackson'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.4.1'
+release = 'v0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
